fd813345fe29071e264312797e5d90b88749f876
Xyven1 <maritimus987@gmail.com>
2023-03-29 17:04:51 -0400

    reformat code to intellij standards

--- src/main/java/edu/wpi/punchy_pegasi/FoodServiceRequestEntry.java
+++ src/main/java/edu/wpi/punchy_pegasi/FoodServiceRequestEntry.java
@@ -3,28 +3,28 @@ package edu.wpi.punchy_pegasi;
 import java.util.List;
 
 public class FoodServiceRequestEntry {
-  private final String foodSelection;
-  private final String tempType;
-  private final List<String> additionalItems;
-  private final String dietaryRestrictions;
-  private final String patientName;
-  private final String roomName;
-  private final String additionalNotes;
+    private final String foodSelection;
+    private final String tempType;
+    private final List<String> additionalItems;
+    private final String dietaryRestrictions;
+    private final String patientName;
+    private final String roomName;
+    private final String additionalNotes;
 
-  public FoodServiceRequestEntry(
-      String patientName,
-      String roomName,
-      String additionalNotes,
-      String foodSelection,
-      String tempType,
-      List<String> additionalItems,
-      String dietaryRestrictions) {
-    this.foodSelection = foodSelection;
-    this.tempType = tempType;
-    this.additionalItems = additionalItems;
-    this.dietaryRestrictions = dietaryRestrictions;
-    this.patientName = patientName;
-    this.roomName = roomName;
-    this.additionalNotes = additionalNotes;
-  }
+    public FoodServiceRequestEntry(
+            String patientName,
+            String roomName,
+            String additionalNotes,
+            String foodSelection,
+            String tempType,
+            List<String> additionalItems,
+            String dietaryRestrictions) {
+        this.foodSelection = foodSelection;
+        this.tempType = tempType;
+        this.additionalItems = additionalItems;
+        this.dietaryRestrictions = dietaryRestrictions;
+        this.patientName = patientName;
+        this.roomName = roomName;
+        this.additionalNotes = additionalNotes;
+    }
 }
