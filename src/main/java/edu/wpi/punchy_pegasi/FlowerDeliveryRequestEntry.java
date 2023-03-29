8e70df0fa0c04d951741d21bd135aa8b651c204d
Coops530 <cooper530@users.noreply.github.com>
2023-03-29 19:31:49 -0400

    Added functionality for FlowerDeliveryRequest submit button.

--- src/main/java/edu/wpi/punchy_pegasi/FlowerDeliveryRequestEntry.java
+++ src/main/java/edu/wpi/punchy_pegasi/FlowerDeliveryRequestEntry.java
@@ -2,73 +2,35 @@ package edu.wpi.punchy_pegasi;
 
 public class FlowerDeliveryRequestEntry {
 
-    private final String patientName;
-    private final String additionalNotes;
-    private final String flowerSize;
-    private final String flowerType;
-    private final int roomNumber;
-    private final int flowerAmount;
+  private final String patientName;
+  private final String additionalNotes;
+  private final String flowerSize;
+  private final String flowerType;
+  private final String roomNumber;
+  private final String flowerAmount;
 
-    public FlowerDeliveryRequestEntry(
-            String patientName,
-            String additionalNotes,
-            String flowerSize,
-            String roomNumber,
-            String flowerAmount,
-            String flowerType) {
-        this.patientName = patientName;
-        this.additionalNotes = additionalNotes;
-        this.flowerSize = flowerSize;
-        this.roomNumber = Integer.parseInt(roomNumber);
-        this.flowerAmount = Integer.parseInt(flowerAmount);
-        this.flowerType = flowerType;
-    }
+  public FlowerDeliveryRequestEntry(
+      String patientName,
+      String additionalNotes,
+      String flowerSize,
+      String roomNumber,
+      String flowerAmount,
+      String flowerType) {
+    this.patientName = patientName;
+    this.additionalNotes = additionalNotes;
+    this.flowerSize = flowerSize;
+    this.roomNumber = roomNumber;
+    this.flowerAmount = flowerAmount;
+    this.flowerType = flowerType;
+  }
 
-    public FlowerDeliveryRequestEntry(
-            String patientName,
-            String flowerSize,
-            String roomNumber,
-            String flowerAmount,
-            String flowerType) {
-        this.patientName = patientName;
-        this.additionalNotes = "";
-        this.flowerSize = flowerSize;
-        this.roomNumber = Integer.parseInt(roomNumber);
-        this.flowerAmount = Integer.parseInt(flowerAmount);
-        this.flowerType = flowerType;
-    }
 
-    public FlowerDeliveryRequestEntry(
-            String patientName,
-            String additionalNotes,
-            String flowerSize,
-            int roomNumber,
-            int flowerAmount,
-            String flowerType) {
-        this.patientName = patientName;
-        this.additionalNotes = additionalNotes;
-        this.flowerSize = flowerSize;
-        this.roomNumber = (roomNumber);
-        this.flowerAmount = (flowerAmount);
-        this.flowerType = flowerType;
-    }
-
-    public FlowerDeliveryRequestEntry(
-            String patientName, String flowerSize, int roomNumber, int flowerAmount, String flowerType) {
-        this.patientName = patientName;
-        this.additionalNotes = "";
-        this.flowerSize = flowerSize;
-        this.roomNumber = (roomNumber);
-        this.flowerAmount = (flowerAmount);
-        this.flowerType = flowerType;
-    }
-
-    public void printFlowerReq() {
-        System.out.println("Patient Name: " + patientName);
-        System.out.println("Additional Notes: " + additionalNotes);
-        System.out.println("Flower Size: " + flowerSize);
-        System.out.println("Room Number: " + roomNumber);
-        System.out.println("Flower Amount: " + flowerAmount);
-        System.out.println("Flower Type: " + flowerType);
-    }
+  public void printFlowerReq() {
+    System.out.println("Patient Name: " + patientName);
+    System.out.println("Additional Notes: " + additionalNotes);
+    System.out.println("Flower Size: " + flowerSize);
+    System.out.println("Room Number: " + roomNumber);
+    System.out.println("Flower Amount: " + flowerAmount);
+    System.out.println("Flower Type: " + flowerType);
+  }
 }
