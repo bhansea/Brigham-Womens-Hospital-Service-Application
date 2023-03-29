849b537c0c2b8e63671bfd344be793005b48be0d
Coops530 <cooper530@users.noreply.github.com>
2023-03-29 14:45:31 -0400

    Changed sizing of FoodServiceRequest page to be dynamic.

--- src/main/java/edu/wpi/punchy_pegasi/controllers/FoodServiceRequestController.java
+++ src/main/java/edu/wpi/punchy_pegasi/controllers/FoodServiceRequestController.java
@@ -4,6 +4,7 @@ import edu.wpi.punchy_pegasi.FoodServiceRequestEntry;
 import edu.wpi.punchy_pegasi.navigation.Navigation;
 import edu.wpi.punchy_pegasi.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
+import java.util.ArrayList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -11,74 +12,80 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
-import java.util.ArrayList;
-
 public class FoodServiceRequestController {
-    FoodServiceRequestEntry entry;
+  FoodServiceRequestEntry entry;
 
-    @FXML
-    TextField dietaryRestrictions;
-    @FXML
-    TextField patientName;
-    @FXML
-    TextField roomNumber;
-    @FXML
-    TextField additionalNotes;
-    @FXML
-    RadioButton hot;
-    @FXML
-    RadioButton warm;
-    @FXML
-    RadioButton cold;
-    @FXML
-    CheckBox utensils;
-    @FXML
-    CheckBox napkins;
-    @FXML
-    CheckBox glass;
-    @FXML
-    MFXComboBox<String> mealDropdown;
+  @FXML TextField dietaryRestrictions;
+  @FXML TextField patientName;
+  @FXML TextField roomNumber;
+  @FXML TextField additionalNotes;
+  @FXML RadioButton hot;
+  @FXML RadioButton warm;
+  @FXML RadioButton cold;
+  @FXML CheckBox utensils;
+  @FXML CheckBox napkins;
+  @FXML CheckBox glass;
+  @FXML MFXComboBox<String> mealDropdown;
 
-    @FXML
-    public void initialize() {
-        ObservableList<String> mealList =
-                FXCollections.observableArrayList(
-                        "Mac and Cheese", "Steak", "Chicken and Rice", "Meatloaf");
-        mealDropdown.setItems(mealList);
+  @FXML
+  public void initialize() {
+    ObservableList<String> mealList =
+        FXCollections.observableArrayList(
+            "Mac and Cheese", "Steak", "Chicken and Rice", "Meatloaf");
+    mealDropdown.setItems(mealList);
+  }
+
+  @FXML
+  public void submitEntry() {
+    String tempType = "";
+    ArrayList<String> extras = new ArrayList<String>();
+    if (hot.isSelected()) {
+      tempType = "hot";
+    } else if (warm.isSelected()) {
+      tempType = "cold";
+    } else if (cold.isSelected()) {
+      tempType = "cold";
     }
 
-    @FXML
-    public void submitEntry() {
-        String tempType = "";
-        ArrayList<String> extras = new ArrayList<String>();
-        if (hot.isSelected()) {
-            tempType = "hot";
-        } else if (warm.isSelected()) {
-            tempType = "cold";
-        } else if (cold.isSelected()) {
-            tempType = "cold";
-        }
-
-        if (utensils.isSelected()) {
-            extras.add("utensils");
-        }
-        if (napkins.isSelected()) {
-            extras.add("napkins");
-        }
-        if (glass.isSelected()) {
-            extras.add("glass");
-        }
-
-        entry =
-                new FoodServiceRequestEntry(
-                        patientName.getText(),
-                        roomNumber.getText(),
-                        additionalNotes.getText(),
-                        mealDropdown.getSelectedItem(),
-                        tempType,
-                        extras,
-                        dietaryRestrictions.getText());
-
-        Navigation.navigate(Screen.HOME);
+    if (utensils.isSelected()) {
+      extras.add("utensils");
     }
+    if (napkins.isSelected()) {
+      extras.add("napkins");
+    }
+    if (glass.isSelected()) {
+      extras.add("glass");
+    }
+
+    String name;
+    String room;
+    String notes;
+    String restrictions;
+
+    try {
+      name = patientName.getText();
+    } catch (NullPointerException e) {
+      name = "";
+    }
+    try {
+      room = roomNumber.getText();
+    } catch (NullPointerException e) {
+      room = "";
+    }
+    try {
+      notes = additionalNotes.getText();
+    } catch (NullPointerException e) {
+      notes = "";
+    }
+    try {
+      restrictions = dietaryRestrictions.getText();
+    } catch (NullPointerException e) {
+      restrictions = "";
+    }
+
+    entry =
+        new FoodServiceRequestEntry(
+            name, room, notes, mealDropdown.getSelectedItem(), tempType, extras, restrictions);
+    Navigation.navigate(Screen.HOME);
+  }
 }
