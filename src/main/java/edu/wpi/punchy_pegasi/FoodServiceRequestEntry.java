e87b05bcceb7a99c19a590313af4c21185124ff3
Coops530 <cooper530@users.noreply.github.com>
2023-03-28 20:24:07 -0400

    Completed drop-down utility, updated FoodServiceRequest class.

---
+++ src/main/java/edu/wpi/punchy_pegasi/FoodServiceRequestEntry.java
@@ -0,0 +1,15 @@
+package edu.wpi.punchy_pegasi;
+
+import java.util.List;
+
+public class FoodServiceRequestEntry {
+  private String foodSelection;
+  private String tempType;
+  private List<String> additionalItems;
+  private String dietaryRestrictions;
+  private String patientName;
+  private String roomName;
+  private String additionalNotes;
+
+  public FoodServiceRequestEntry() {}
+}
