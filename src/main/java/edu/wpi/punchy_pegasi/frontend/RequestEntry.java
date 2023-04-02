149265476573fb25332e0e0e9bc1e429ce24ba7b
Coops530 <cooper530@users.noreply.github.com>
2023-04-02 16:24:06 -0400

    Added enum attribute for status of request.

--- src/main/java/edu/wpi/punchy_pegasi/frontend/RequestEntry.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/RequestEntry.java
@@ -5,11 +5,18 @@ public class RequestEntry {
     protected final String roomNumber;
     protected final String staffAssignment;
     protected final String additionalNotes;
+    protected enum Status {
+        NONE,
+        PROCESSING,
+        DONE
+    }
+    protected final Status status;
 
     public RequestEntry(String patientName, String roomNumber, String staffAssignment, String additionalNotes) {
         this.patientName = patientName;
         this.roomNumber = roomNumber;
         this.staffAssignment = staffAssignment;
         this.additionalNotes = additionalNotes;
+        this.status = Status.PROCESSING;
     }
 }
