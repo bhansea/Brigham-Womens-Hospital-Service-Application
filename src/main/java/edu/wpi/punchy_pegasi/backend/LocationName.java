5aa78132cc1a7b5df04727a3574e8ef1890a0a5f
Xyven1 <maritimus987@gmail.com>
2023-04-02 02:13:31 -0400

    intellij format and optimize code

--- src/main/java/edu/wpi/punchy_pegasi/backend/LocationName.java
+++ src/main/java/edu/wpi/punchy_pegasi/backend/LocationName.java
@@ -3,13 +3,14 @@ package edu.wpi.punchy_pegasi.backend;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 public class LocationName {
     private String longName;
     private String shortName;
     private NodeType nodeType;
 
-    public LocationName(String longName, String shortName, NodeType nodeType){
+    public LocationName(String longName, String shortName, NodeType nodeType) {
         this.longName = longName;
         this.shortName = shortName;
         this.nodeType = nodeType;
--- src/main/java/edu/wpi/punchy_pegasi/backend/Move.java
+++ src/main/java/edu/wpi/punchy_pegasi/backend/Move.java
@@ -3,16 +3,15 @@ package edu.wpi.punchy_pegasi.backend;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
-
-@Getter@Setter
+@Getter
+@Setter
 public class Move {
     private long uuid;
     private long nodeID;
     private String longName;
     private String date;
 
-    Move(long uuid, long nodeID, String longName, String date){
+    Move(long uuid, long nodeID, String longName, String date) {
         this.uuid = uuid;
         this.nodeID = nodeID;
         this.longName = longName;
