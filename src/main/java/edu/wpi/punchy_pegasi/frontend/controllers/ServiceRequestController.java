dc24dc38a734b9a5af6a0538335a1db3e2dad01c
charcar515 <106764357+charcar515@users.noreply.github.com>
2023-04-01 21:29:32 -0400

    started showReq function

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/ServiceRequestController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/ServiceRequestController.java
@@ -3,15 +3,20 @@ package edu.wpi.punchy_pegasi.frontend.controllers;
 import edu.wpi.punchy_pegasi.frontend.navigation.Navigation;
 import edu.wpi.punchy_pegasi.frontend.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 
 public class ServiceRequestController {
 
     @FXML
-    MFXButton backButton;
+    MFXTextField servSearchBar;
+    @FXML
+    void showReq(){
+
+    }
 
     @FXML
     public void initialize() {
-        backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+        servSearchBar.setOnKeyPressed(event -> Navigation.navigate(Screen.HOME));
     }
 }
