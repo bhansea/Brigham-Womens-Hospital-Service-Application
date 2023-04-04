26c95dc1982bddaaaef3aea9b6bfea0874bd5d4d
tommywwz <wangweizhe2001@gmail.com>
2023-04-04 19:34:14 -0400

    Create FlowerDeliveryRequestEntryDaoImplTest.java

---
+++ src/test/java/edu/wpi/punchy_pegasi/generated/FlowerDeliveryRequestEntryDaoImplTest.java
@@ -0,0 +1,29 @@
+package edu.wpi.punchy_pegasi.generated;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlowerDeliveryRequestEntryDaoImplTest {
+
+    @Test
+    void get() {
+        var dao = new FlowerDeliveryRequestEntryDaoImpl();
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
