2e3d8e60f2afb323552e01e6d7caa6b2f2e11f16
Piper O'Connell <proconnell@wpi.edu>
2023-04-04 15:51:51 -0400

    class updates

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/ConferenceRoomController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/ConferenceRoomController.java
@@ -1,8 +1,8 @@
 package edu.wpi.punchy_pegasi.frontend.controllers.requests;
 
-import edu.wpi.punchy_pegasi.schema.ConferenceRoomEntry;
 import edu.wpi.punchy_pegasi.frontend.navigation.Navigation;
 import edu.wpi.punchy_pegasi.frontend.navigation.Screen;
+import edu.wpi.punchy_pegasi.schema.ConferenceRoomEntry;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,85 +11,73 @@ import javafx.scene.layout.BorderPane;
 
 public class ConferenceRoomController extends RequestController<ConferenceRoomEntry> {
 
-    @FXML MFXComboBox<String> beginningTime;
-    @FXML MFXComboBox<String> endTime;
+    @FXML
+    MFXComboBox<String> beginningTime;
+    @FXML
+    MFXComboBox<String> endTime;
+
+    private ObservableList<String> timeList = FXCollections.observableArrayList("12:00am", "12:30am", "1:00am", "1:30am",
+            "2:00am", "2:30am",
+            "3:00am", "3:30am",
+            "4:00am", "4:30am",
+            "5:00am", "5:30am",
+            "6:00am", "6:30am",
+            "7:00am", "7:30am",
+            "8:00am", "8:30am",
+            "9:00am", "9:30am",
+            "10:00am", "10:30am",
+            "11:00am", "11:30am",
+            "12:00pm", "12:30pm",
+            "12:00pm", "12:30pm",
+            "1:00pm", "1:30pm",
+            "2:00pm", "2:30pm",
+            "3:00pm", "3:30pm",
+            "4:00pm", "4:30pm",
+            "5:00pm", "5:30pm",
+            "6:00pm", "6:30pm",
+            "7:00pm", "7:30pm",
+            "8:00pm", "8:30pm",
+            "9:00pm", "9:30pm",
+            "10:00pm", "10:30pm",
+            "11:00pm", "11:30pm");
 
 
-    public static BorderPane create() {
-        return RequestController.create(new ConferenceRoomController(), "views/ConferenceRoomRequest.fxml");
+    public static BorderPane create(String path) {
+        return RequestController.create(new ConferenceRoomController(), path);
     }
 
     @FXML
     public void init() {
-        //beginningTime.setItems(begTime);
-        //endingTime.setItems(endTime);
-        ObservableList<String> timeList = FXCollections.observableArrayList("12:00am","12:30am","1:00am", "1:30am",
-                "2:00am", "2:30am",
-                "3:00am", "3:30am",
-                "4:00am", "4:30am",
-                "5:00am", "5:30am",
-                "6:00am", "6:30am",
-                "7:00am", "7:30am",
-                "8:00am", "8:30am",
-                "9:00am", "9:30am",
-                "10:00am", "10:30am",
-                "11:00am", "11:30am",
-                "12:00pm", "12:30pm",
-                "12:00pm", "12:30pm",
-                "1:00pm", "1:30pm",
-                "2:00pm", "2:30pm",
-                "3:00pm", "3:30pm",
-                "4:00pm", "4:30pm",
-                "5:00pm", "5:30pm",
-                "6:00pm", "6:30pm",
-                "7:00pm", "7:30pm",
-                "8:00pm", "8:30pm",
-                "9:00pm", "9:30pm",
-                "10:00pm", "10:30pm",
-                "11:00pm", "11:30pm");
         beginningTime.setItems(timeList);
         endTime.setItems(timeList);
         endTime.setDisable(true);
         submit.setDisable(true);
+        beginningTime.setOnCommit(e->validateEntry());
+        endTime.setOnCommit(e-> validateEntry());
+    }
+
+    @Override
+    protected void clearEntry() {
+        clearGeneric();
+        beginningTime.clear();
+        endTime.clear();
     }
 
     //validates the bottom box if the front one is filled in
-    @FXML public void validateField(){
+    @FXML
+    public void validateField() {
         boolean selected = beginningTime.getSelectedItem() == null;
         endTime.setDisable(selected);
     }
 
-    @FXML public void validateEntry(){
-        boolean val = validateGeneric() || timeCheck();
+    @Override
+    protected void validateEntry() {
+        boolean val = validateGeneric() || !timeCheck();
         submit.setDisable(val);
     }
 
     //will check to make sure that the end time is after the beginning time
-    public boolean timeCheck(){
-        ObservableList<String> timeList = FXCollections.observableArrayList("12:00am","12:30am","1:00am", "1:30am",
-                "2:00am", "2:30am",
-                "3:00am", "3:30am",
-                "4:00am", "4:30am",
-                "5:00am", "5:30am",
-                "6:00am", "6:30am",
-                "7:00am", "7:30am",
-                "8:00am", "8:30am",
-                "9:00am", "9:30am",
-                "10:00am", "10:30am",
-                "11:00am", "11:30am",
-                "12:00pm", "12:30pm",
-                "12:00pm", "12:30pm",
-                "1:00pm", "1:30pm",
-                "2:00pm", "2:30pm",
-                "3:00pm", "3:30pm",
-                "4:00pm", "4:30pm",
-                "5:00pm", "5:30pm",
-                "6:00pm", "6:30pm",
-                "7:00pm", "7:30pm",
-                "8:00pm", "8:30pm",
-                "9:00pm", "9:30pm",
-                "10:00pm", "10:30pm",
-                "11:00pm", "11:30pm");
+    public boolean timeCheck() {
         String btHolder = beginningTime.getSelectedItem();
         String etHolder = endTime.getSelectedItem();
         return timeList.indexOf(btHolder) < timeList.indexOf(etHolder);
