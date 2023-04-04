4c8f61100a012b85fbc9f2ed2a4c3b8381863275
Piper O'Connell <proconnell@wpi.edu>
2023-04-03 21:33:05 -0400

    everything in pr

--- src/main/java/edu/wpi/punchy_pegasi/frontend/ConferenceRoomEntry.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/ConferenceRoomEntry.java
@@ -1,20 +1,42 @@
 package edu.wpi.punchy_pegasi.frontend;
 
+import java.util.UUID;
+
 public class ConferenceRoomEntry extends RequestEntry {
 
     private final String beginningTime;
     private final String endTime;
 
     //String patientName, String roomNumber, String additionalNotes
-    public ConferenceRoomEntry(String beginningTime,
-                               String endTime,
+    public ConferenceRoomEntry(UUID serviceID,
                                String patientName,
                                String roomNumber,
                                String additionalNotes,
-                               String staffAssignment){
-        super(patientName,roomNumber,additionalNotes,staffAssignment);
+                               String staffAssignment,
+                               Status status,
+                               String beginningTime,
+                               String endTime){
+        super(serviceID, patientName, roomNumber,additionalNotes,staffAssignment,status);
         this.beginningTime = beginningTime;
         this.endTime = endTime;
+    }
+
+
+    public ConferenceRoomEntry(
+            String patientName,
+            String roomNumber,
+            String staffAssignment,
+            String additionalNotes,
+            String beginningTime,
+            String endTime) {
+        this(UUID.randomUUID(),
+                patientName,
+                roomNumber,
+                staffAssignment,
+                additionalNotes,
+                Status.PROCESSING,
+                beginningTime,
+                endTime);
     }
 
 }
--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/ConferenceRoomController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/ConferenceRoomController.java
@@ -58,17 +58,46 @@ public class ConferenceRoomController extends RequestController<ConferenceRoomEn
         beginningTime.setItems(timeList);
         endTime.setItems(timeList);
         endTime.setDisable(true);
-        validateField();
+        submit.setDisable(true);
     }
 
     //validates the bottom box if the front one is filled in
     @FXML public void validateField(){
-        Boolean selected = beginningTime.getSelectedItem() == null;
+        boolean selected = beginningTime.getSelectedItem() == null;
         endTime.setDisable(selected);
     }
 
+    @FXML public void validateEntry(){
+        boolean val = validateGeneric() || timeCheck();
+        submit.setDisable(val);
+    }
+
     //will check to make sure that the end time is after the beginning time
-    public boolean timeCheck(ObservableList<String> timeList){
+    public boolean timeCheck(){
+        ObservableList<String> timeList = FXCollections.observableArrayList("12:00am","12:30am","1:00am", "1:30am",
+                "2:00am", "2:30am",
+                "3:00am", "3:30am",
+                "4:00am", "4:30am",
+                "5:00am", "5:30am",
+                "6:00am", "6:30am",
+                "7:00am", "7:30am",
+                "8:00am", "8:30am",
+                "9:00am", "9:30am",
+                "10:00am", "10:30am",
+                "11:00am", "11:30am",
+                "12:00pm", "12:30pm",
+                "12:00pm", "12:30pm",
+                "1:00pm", "1:30pm",
+                "2:00pm", "2:30pm",
+                "3:00pm", "3:30pm",
+                "4:00pm", "4:30pm",
+                "5:00pm", "5:30pm",
+                "6:00pm", "6:30pm",
+                "7:00pm", "7:30pm",
+                "8:00pm", "8:30pm",
+                "9:00pm", "9:30pm",
+                "10:00pm", "10:30pm",
+                "11:00pm", "11:30pm");
         String btHolder = beginningTime.getSelectedItem();
         String etHolder = endTime.getSelectedItem();
         return timeList.indexOf(btHolder) < timeList.indexOf(etHolder);
@@ -78,12 +107,12 @@ public class ConferenceRoomController extends RequestController<ConferenceRoomEn
     public void submitEntry() {
         requestEntry =
                 new ConferenceRoomEntry(
-                        patientName.getText(),
+                        null,
                         roomNumber.getText(),
+                        staffAssignment.getText(),
                         additionalNotes.getText(),
                         beginningTime.getText(),
-                        endTime.getText(),
-                        staffAssignment.getText());
+                        endTime.getText());
         Navigation.navigate(Screen.HOME);
     }
 
