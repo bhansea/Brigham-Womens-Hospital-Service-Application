13d1119254833d4caf7ac908d3cd542d2ce3bd5c
Arnav Mishra <49102795+arnavm21@users.noreply.github.com>
2023-04-04 23:04:57 -0400

    Test Classes
    Created Test Classes

--- src/test/java/edu/wpi/punchy_pegasi/generated/FlowerDeliveryRequestEntryDaoImplTest.java
+++ src/test/java/edu/wpi/punchy_pegasi/generated/FlowerDeliveryRequestEntryDaoImplTest.java
@@ -1,10 +1,22 @@
 package edu.wpi.punchy_pegasi.generated;
 
+import edu.wpi.punchy_pegasi.backend.PdbController;
+import edu.wpi.punchy_pegasi.schema.TableType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlowerDeliveryRequestEntryDaoImplTest {
+    private final PdbController pdb = new PdbController("jdbc:postgresql://database.cs.wpi.edu:5432/teampdb", "teamp", "teamp130");
+    @BeforeAll
+    void init(){
+        try {
+            pdb.initTableByType(TableType.FLOWERREQUESTS);
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     void get() {
