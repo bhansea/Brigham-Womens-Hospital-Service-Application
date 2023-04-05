708074a2db200b58d179fd02c7f7c5971e1247bc
Arnav Mishra <49102795+arnavm21@users.noreply.github.com>
2023-04-04 23:33:08 -0400

    TestFileUpdates
    Initialized tests

--- src/test/java/edu/wpi/punchy_pegasi/generated/FlowerDeliveryRequestEntryDaoImplTest.java
+++ src/test/java/edu/wpi/punchy_pegasi/generated/FlowerDeliveryRequestEntryDaoImplTest.java
@@ -1,18 +1,29 @@
 package edu.wpi.punchy_pegasi.generated;
 
 import edu.wpi.punchy_pegasi.backend.PdbController;
+import edu.wpi.punchy_pegasi.schema.FlowerDeliveryRequestEntry;
+import edu.wpi.punchy_pegasi.schema.RequestEntry;
 import edu.wpi.punchy_pegasi.schema.TableType;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlowerDeliveryRequestEntryDaoImplTest {
-    private final PdbController pdb = new PdbController("jdbc:postgresql://database.cs.wpi.edu:5432/teampdb", "teamp", "teamp130");
+    PdbController pdbController;
+
     @BeforeAll
     void init(){
+        pdbController = new PdbController("jdbc:postgresql://database.cs.wpi.edu:5432/teampdb", "teamp", "teamp130");
         try {
-            pdb.initTableByType(TableType.FLOWERREQUESTS);
+            pdbController.initTableByType(TableType.FLOWERREQUESTS);
         } catch (PdbController.DatabaseException e) {
             throw new RuntimeException(e);
         }
@@ -20,7 +31,14 @@ class FlowerDeliveryRequestEntryDaoImplTest {
 
     @Test
     void get() {
-        var dao = new FlowerDeliveryRequestEntryDaoImpl();
+        var dao = new FlowerDeliveryRequestEntryDaoImpl(pdbController);
+        FlowerDeliveryRequestEntry flowers = new FlowerDeliveryRequestEntry(UUID.randomUUID(),"testPatient", "testRoomNum", "testStaff", "testNotes", RequestEntry.Status.PROCESSING, "testSmall", "test1", "testTulip");
+        dao.save(flowers);
+        try {
+            ResultSet result = pdbController.searchQuery(TableType.FLOWERREQUESTS, "serviceID",flowers.getServiceID());
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
--- src/test/java/edu/wpi/punchy_pegasi/generated/NodeDaoImplTest.java
+++ src/test/java/edu/wpi/punchy_pegasi/generated/NodeDaoImplTest.java
@@ -1,10 +1,24 @@
 package edu.wpi.punchy_pegasi.generated;
 
+import edu.wpi.punchy_pegasi.backend.PdbController;
+import edu.wpi.punchy_pegasi.schema.TableType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NodeDaoImplTest {
+    PdbController pdbController;
+
+    @BeforeAll
+    void init(){
+        pdbController = new PdbController("jdbc:postgresql://database.cs.wpi.edu:5432/teampdb", "teamp", "teamp130");
+        try {
+            pdbController.initTableByType(TableType.NODES);
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     void get() {
