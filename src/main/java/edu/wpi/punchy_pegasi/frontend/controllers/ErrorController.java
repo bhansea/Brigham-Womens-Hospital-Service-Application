90a3e0075f851d75e5d575537d1a57a928e9c43e
Xyven1 <maritimus987@gmail.com>
2023-04-07 02:06:48 -0400

    added component for displaying errors

---
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/ErrorController.java
@@ -0,0 +1,18 @@
+package edu.wpi.punchy_pegasi.frontend.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ErrorController {
+    @FXML
+    private Label errorMessage;
+
+    public void setErrorText(String text) {
+        errorMessage.setText(text);
+    }
+
+    @FXML
+    private void close() {
+        errorMessage.getScene().getWindow().hide();
+    }
+}
