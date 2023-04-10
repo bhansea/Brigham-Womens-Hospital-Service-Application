8e62d73021e188125c9cc847d3666f62e53c1a79
RyanNguyen321 <nguyenryan0903@gmail.com>
2023-04-10 11:55:09 -0400

    Fixed admin page display table code.

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/AdminPageController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/AdminPageController.java
@@ -558,30 +558,4 @@ public class AdminPageController {
         moveTable.setManaged(false);
         locationTable.setManaged(false);
     }
-
-
-    @FXML
-    String showReq() {
-        String temp = "";
-        if (submit.isPressed()) {
-            temp = servSearchBar.getText();
-            for (int i = 0; i < requests.size(); i++) { //does not loop because theres nothing in requests so no size
-                //so i++ is never used as well
-                if (temp.matches(requests.get(i))) {
-                    return requests.get(i);
-                } else {
-                    throw new RuntimeException("No such request found.");
-                }
-            }
-        }
-
-        //match case
-        //throw error if no request found
-        //display request
-        //display back button/somehow reset the search bar for a new search?
-
-        return temp;
-
-
-    }
 }
