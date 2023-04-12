a25798d5001dc69cb1f2a3887112a24510cab0b2
SupermanBritt <supermanbritt03@comcast.net>
2023-04-11 22:56:02 -0400

    Update HomePageController.java
    fixed more but still broken

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/HomePageController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/HomePageController.java
@@ -2,6 +2,7 @@ package edu.wpi.punchy_pegasi.frontend.controllers;
 
 import edu.wpi.punchy_pegasi.App;
 import edu.wpi.punchy_pegasi.frontend.Screen;
+import edu.wpi.punchy_pegasi.generated.Facade;
 import edu.wpi.punchy_pegasi.generated.FlowerDeliveryRequestEntryDaoImpl;
 import edu.wpi.punchy_pegasi.schema.FlowerDeliveryRequestEntry;
 import edu.wpi.punchy_pegasi.schema.RequestEntry;
@@ -18,9 +19,12 @@ import javafx.scene.layout.VBox;
 import java.util.Comparator;
 
 public class HomePageController {
-    @FXML MFXTableView<RequestEntry> requestTable;
+    @FXML
+    MFXTableView<RequestEntry> requestTable;
     @FXML
     MFXTableView<RequestEntry> openRequestTable;
+
+    private Facade facade = App.getSingleton().getFacade();
 
     @FXML
     public void initialize() {
@@ -33,13 +37,19 @@ public class HomePageController {
     }
 
     public void initRequestTable() {
-        RequestEntryDaoImpl requestDaoImpl = new RequestEntryDaoImpl();
-        ObservableList<RequestEntry> requestList = FXCollections.observableArrayList(requestDaoImpl.getAll().values());
-
-        for (RequestEntry.Status status : RequestEntry.Status.values()) {
-            MFXTableColumn<RequestEntry> col = new MFXTableColumn<>(status.name(), true, Comparator.comparing(RequestEntry::getServiceID));
-            col.setRowCellFactory(p -> new MFXTableRowCell<>(status::valueOf));
-            requestTable.getTableColumns().add(col);
+        facade.getAllFurnitureRequestEntry();
+        facade.getAllConferenceRoomEntry();
+        facade.getAllFlowerDeliveryRequestEntry();
+        facade.getAllOfficeServiceRequestEntry();
+        ObservableList<RequestEntry> requestList = FXCollections.observableArrayList(facade.getAllFurnitureRequestEntry().values());
+        requestList.addAll(facade.getAllConferenceRoomEntry().values());
+        requestList.addAll(facade.getAllFlowerDeliveryRequestEntry().values());
+        requestList.addAll(facade.getAllOfficeServiceRequestEntry().values());
+        //FIX THIS
+        for (RequestEntry.Field field : RequestEntry.Field.values()) {
+            MFXTableColumn<RequestEntry> col = new MFXTableColumn<>(field.getColName(), true, Comparator.comparing(FlowerDeliveryRequestEntry::getServiceID));
+            col.setRowCellFactory(p -> new MFXTableRowCell<>(field::getValue));
+            flowerServiceRequestTable.getTableColumns().add(col);
         }
         requestTable.setItems(requestList);
     }
