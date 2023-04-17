113ba947c6b6e4b6f75e9c7a2956540b7fcb3417
bhansea <97768606+bhansea@users.noreply.github.com>
2023-04-17 13:35:38 -0400

    finished get tests for Node

--- src/test/java/edu/wpi/punchy_pegasi/generated/FacadeTest.java
+++ src/test/java/edu/wpi/punchy_pegasi/generated/FacadeTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -83,14 +85,115 @@ class FacadeTest {
 
     @Test
     void testGetNode() {
+        Node node = new Node(100L, 500, 500, "L1", "testBuilding");
+        Node node2 = new Node(101L, 500, 500, "L1", "testBuilding");
+        Object[] values = new Object[]{node.getNodeID(), node.getXcoord(), node.getYcoord(), node.getFloor(), node.getBuilding()};
+        Object[] values2 = new Object[]{node2.getNodeID(), node2.getXcoord(), node2.getYcoord(), node2.getFloor(), node2.getBuilding()};
+        try {
+            pdbController.insertQuery(TableType.NODES, nodeFields, values);
+            pdbController.insertQuery(TableType.NODES, nodeFields, values2);
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+        var results = nodeDao.get(Node.Field.BUILDING, "testBuilding");
+        var map = new HashMap<Long, Node>();
+        try (var rs = pdbController.searchQuery(TableType.NODES, "building", "testBuilding")) {
+            while (rs.next()) {
+                Node req = new Node(
+                        (java.lang.Long) rs.getObject("nodeID"),
+                        (java.lang.Integer) rs.getObject("xcoord"),
+                        (java.lang.Integer) rs.getObject("ycoord"),
+                        (java.lang.String) rs.getObject("floor"),
+                        (java.lang.String) rs.getObject("building"));
+                if (req != null)
+                    map.put(req.getNodeID(), req);
+            }
+        } catch (PdbController.DatabaseException | SQLException e) {
+            log.error("", e);
+        }
+        assertEquals(map.get(node.getNodeID()), results.get(node.getNodeID()));
+        assertEquals(map.get(node2.getNodeID()), results.get(node2.getNodeID()));
+        try {
+            pdbController.deleteQuery(TableType.NODES, "nodeID", node.getNodeID());
+            pdbController.deleteQuery(TableType.NODES, "nodeID", node2.getNodeID());
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void testGetNode1() {
+        Node node = new Node(100L, 500, 500, "L1", "testBuilding");
+        Node node2 = new Node(101L, 400, 500, "L1", "testBuilding");
+        Node node3 = new Node(102L, 500, 500, "L1", "testBuilding1");
+        Object[] values = new Object[]{node.getNodeID(), node.getXcoord(), node.getYcoord(), node.getFloor(), node.getBuilding()};
+        Object[] values2 = new Object[]{node2.getNodeID(), node2.getXcoord(), node2.getYcoord(), node2.getFloor(), node2.getBuilding()};
+        Object[] values3 = new Object[]{node3.getNodeID(), node3.getXcoord(), node3.getYcoord(), node3.getFloor(), node3.getBuilding()};
+        try {
+            pdbController.insertQuery(TableType.NODES, nodeFields, values);
+            pdbController.insertQuery(TableType.NODES, nodeFields, values2);
+            pdbController.insertQuery(TableType.NODES, nodeFields, values3);
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+        Node.Field[] fields = {Node.Field.BUILDING, Node.Field.XCOORD};
+        Object[] searchValues = new Object[]{"testBuilding", 500};
+        String[] searchFields = new String[]{"building", "xcoord"};
+        var results = nodeDao.get(fields, searchValues);
+        var map = new HashMap<Long, Node>();
+        try (var rs = pdbController.searchQuery(TableType.NODES, searchFields, searchValues)) {
+            while (rs.next()) {
+                Node req = new Node(
+                        (java.lang.Long) rs.getObject("nodeID"),
+                        (java.lang.Integer) rs.getObject("xcoord"),
+                        (java.lang.Integer) rs.getObject("ycoord"),
+                        (java.lang.String) rs.getObject("floor"),
+                        (java.lang.String) rs.getObject("building"));
+                if (req != null)
+                    map.put(req.getNodeID(), req);
+            }
+        } catch (PdbController.DatabaseException | SQLException e) {
+            log.error("", e);
+        }
+        assertEquals(map.get(node.getNodeID()), results.get(node.getNodeID()));
+        assertEquals(map.get(node2.getNodeID()), results.get(node2.getNodeID()));
+        assertEquals(map.get(node3.getNodeID()), results.get(node3.getNodeID()));
+        try {
+            pdbController.deleteQuery(TableType.NODES, "nodeID", node.getNodeID());
+            pdbController.deleteQuery(TableType.NODES, "nodeID", node2.getNodeID());
+            pdbController.deleteQuery(TableType.NODES, "nodeID", node3.getNodeID());
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void getAllNode() {
+        var values0 = new Object[]{100L, 500, 500, "L1", "testBuilding0"};
+        var values1 = new Object[]{101L, 501, 501, "L1", "testBuilding1"};
+        var values2 = new Object[]{102L, 502, 502, "L1", "testBuilding2"};
+        var valueSet = new Object[][]{values0, values1, values2};
+
+        var refMap = new HashMap<Long, Node>();
+        for (var values : valueSet) {
+            try {
+                pdbController.insertQuery(TableType.NODES, nodeFields, values);
+            } catch (PdbController.DatabaseException e) {
+                throw new RuntimeException(e);
+            }
+            var node = new Node((Long) values[0], (Integer) values[1], (Integer) values[2], (String) values[3], (String) values[4]);
+            refMap.put(node.getNodeID(), node);
+        }
+
+        Map<Long, Node> resultMap = nodeDao.getAll();
+        for (var key : resultMap.keySet()) {
+            try {
+                pdbController.deleteQuery(TableType.NODES, "nodeID", key);
+            } catch (PdbController.DatabaseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        assertEquals(refMap, resultMap);
     }
 
     @Test
