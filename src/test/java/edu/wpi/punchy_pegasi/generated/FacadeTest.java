3d5ff9e12beab30dd97d2231887f0e94551d92c6
bhansea <97768606+bhansea@users.noreply.github.com>
2023-04-18 15:21:40 -0400

    finished FoodServiceRequestEntry tests in FacadeTest changed uuid_generate_v4() to gen_random_uuid()

--- src/test/java/edu/wpi/punchy_pegasi/generated/FacadeTest.java
+++ src/test/java/edu/wpi/punchy_pegasi/generated/FacadeTest.java
@@ -854,7 +854,7 @@ class FacadeTest {
         List<String> additionalItems = new ArrayList<>();
         additionalItems.add("testItems");
         FoodServiceRequestEntry food = new FoodServiceRequestEntry(UUID.randomUUID(), locName, staff, "testNotes", RequestEntry.Status.PROCESSING, "testFood", "testTemp", additionalItems, "juice", "testRestrictions", "testPatient");
-        Object[] values = new Object[]{food.getServiceID(), food.getLocationName(), food.getStaffAssignment(), food.getAdditionalNotes(), food.getStatus(), food.getFoodSelection(), food.getTempType(), food.getAdditionalItems(), food.getDietaryRestrictions(), food.getPatientName()};
+        Object[] values = new Object[]{food.getServiceID(), food.getLocationName(), food.getStaffAssignment(), food.getAdditionalNotes(), food.getStatus(), food.getFoodSelection(), food.getTempType(), food.getAdditionalItems(), food.getBeverage(),food.getDietaryRestrictions(), food.getPatientName()};
         try {
             pdbController.insertQuery(TableType.FOODREQUESTS, foodServiceFields, values);
         } catch (PdbController.DatabaseException e) {
@@ -872,30 +872,292 @@ class FacadeTest {
 
     @Test
     void testGetFoodServiceRequestEntry() {
+        List<String> additionalItems = new ArrayList<>();
+        additionalItems.add("testItems");
+        var locName0 = ThreadLocalRandom.current().nextLong();
+        var staff0 = ThreadLocalRandom.current().nextLong();
+        var locName1 = ThreadLocalRandom.current().nextLong();
+        var staff1 = ThreadLocalRandom.current().nextLong();
+        var food = new FoodServiceRequestEntry(UUID.randomUUID(), locName0, staff0, "testNotes", RequestEntry.Status.PROCESSING, "testFood", "testTemp", additionalItems, "juice", "testRestrictions", "testPatient");
+        var food2 = new FoodServiceRequestEntry(UUID.randomUUID(), locName1, staff1, "testNotes", RequestEntry.Status.PROCESSING, "testFood", "testTemp", additionalItems, "juice", "testRestrictions", "testPatient");
+        var values = new Object[]{food.getServiceID(), food.getLocationName(), food.getStaffAssignment(), food.getAdditionalNotes(), food.getStatus(), food.getFoodSelection(), food.getTempType(), food.getAdditionalItems(), food.getBeverage(), food.getDietaryRestrictions(), food.getPatientName()};
+        var values2 = new Object[]{food2.getServiceID(), food2.getLocationName(), food2.getStaffAssignment(), food2.getAdditionalNotes(), food2.getStatus(), food2.getFoodSelection(), food2.getTempType(), food2.getAdditionalItems(), food2.getBeverage(), food2.getDietaryRestrictions(), food2.getPatientName()};
+        try {
+            pdbController.insertQuery(TableType.FOODREQUESTS, foodServiceFields, values);
+            pdbController.insertQuery(TableType.FOODREQUESTS, foodServiceFields, values2);
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+        var results = facade.getFoodServiceRequestEntry(FoodServiceRequestEntry.Field.STAFF_ASSIGNMENT, staff0);
+        var map = new HashMap<java.util.UUID, FoodServiceRequestEntry>();
+        try (var rs = pdbController.searchQuery(TableType.FOODREQUESTS, "staffAssignment", staff0)) {
+            while (rs.next()) {
+                String myCol = rs.getString("additionalItems");
+                List<String> myColumnList = Arrays.asList(myCol.split(","));
+                FoodServiceRequestEntry req = new FoodServiceRequestEntry(
+                        (java.util.UUID) rs.getObject("serviceID"),
+                        (java.lang.Long) rs.getObject("locationName"),
+                        (java.lang.Long) rs.getObject("staffAssignment"),
+                        rs.getObject("additionalNotes", String.class),
+                        edu.wpi.punchy_pegasi.schema.RequestEntry.Status.valueOf((String) rs.getObject("status")),
+                        (java.lang.String) rs.getObject("foodSelection"),
+                        (java.lang.String) rs.getObject("tempType"),
+                        myColumnList,
+                        (java.lang.String) rs.getObject("beverage"),
+                        (java.lang.String) rs.getObject("dietaryRestrictions"),
+                        (java.lang.String) rs.getObject("patientName"));
+                map.put(req.getServiceID(), req);
+            }
+        } catch (PdbController.DatabaseException | SQLException e) {
+            assert false : e.getMessage();
+        }
+        assertEquals(map.get(food.getServiceID()), results.get(food.getServiceID()));
+        assertEquals(map.get(food2.getServiceID()), results.get(food2.getServiceID()));
+        try {
+            pdbController.deleteQuery(TableType.FOODREQUESTS, "serviceID", food.getServiceID());
+            pdbController.deleteQuery(TableType.FOODREQUESTS, "serviceID", food2.getServiceID());
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void testGetFoodServiceRequestEntry1() {
+        List<String> additionalItems = new ArrayList<>();
+        additionalItems.add("testItems");
+        var locName0 = ThreadLocalRandom.current().nextLong();
+        var staff0 = ThreadLocalRandom.current().nextLong();
+        var locName1 = ThreadLocalRandom.current().nextLong();
+        var staff1 = ThreadLocalRandom.current().nextLong();
+        var food = new FoodServiceRequestEntry(UUID.randomUUID(), locName0, staff0, "testNotes1", RequestEntry.Status.PROCESSING, "testFood1", "testTemp1", additionalItems, "juice", "testRestrictions", "testPatient");
+        var food2 = new FoodServiceRequestEntry(UUID.randomUUID(), locName1, staff1, "testNotes2", RequestEntry.Status.PROCESSING, "testFood2", "testTemp2", additionalItems, "juice", "testRestrictions", "testPatient");
+        var values = new Object[]{food.getServiceID(), food.getLocationName(), food.getStaffAssignment(), food.getAdditionalNotes(), food.getStatus(), food.getFoodSelection(), food.getTempType(), food.getAdditionalItems(), food.getBeverage(), food.getDietaryRestrictions(), food.getPatientName()};
+        var values2 = new Object[]{food2.getServiceID(), food2.getLocationName(), food2.getStaffAssignment(), food2.getAdditionalNotes(), food2.getStatus(), food2.getFoodSelection(), food2.getTempType(), food2.getAdditionalItems(), food2.getBeverage(), food2.getDietaryRestrictions(), food2.getPatientName()};
+        try {
+            pdbController.insertQuery(TableType.FOODREQUESTS, foodServiceFields, values);
+            pdbController.insertQuery(TableType.FOODREQUESTS, foodServiceFields, values2);
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+        FoodServiceRequestEntry.Field[] fields = {FoodServiceRequestEntry.Field.ADDITIONAL_NOTES, FoodServiceRequestEntry.Field.FOOD_SELECTION};
+        Object[] searchValues = new Object[]{"testNotes1", "testFood2"};
+        String[] searchFields = new String[]{"additionalNotes", "foodSelection"};
+        var results = facade.getFoodServiceRequestEntry(fields, searchValues);
+        var map = new HashMap<java.util.UUID, FoodServiceRequestEntry>();
+        try (var rs = pdbController.searchQuery(TableType.FOODREQUESTS, searchFields, searchValues)) {
+            while (rs.next()) {
+                String myCol = rs.getString("additionalItems");
+                List<String> myColumnList = Arrays.asList(myCol.split(","));
+                FoodServiceRequestEntry req = new FoodServiceRequestEntry(
+                        (java.util.UUID) rs.getObject("serviceID"),
+                        (java.lang.Long) rs.getObject("locationName"),
+                        (java.lang.Long) rs.getObject("staffAssignment"),
+                        rs.getObject("additionalNotes", String.class),
+                        edu.wpi.punchy_pegasi.schema.RequestEntry.Status.valueOf((String) rs.getObject("status")),
+                        (java.lang.String) rs.getObject("foodSelection"),
+                        (java.lang.String) rs.getObject("tempType"),
+                        myColumnList,
+                        (java.lang.String) rs.getObject("beverage"),
+                        (java.lang.String) rs.getObject("dietaryRestrictions"),
+                        (java.lang.String) rs.getObject("patientName"));
+                map.put(req.getServiceID(), req);
+            }
+        } catch (PdbController.DatabaseException | SQLException e) {
+            assert false : e.getMessage();
+        }
+        assertEquals(map.get(food.getServiceID()), results.get(food.getServiceID()));
+        assertEquals(map.get(food2.getServiceID()), results.get(food2.getServiceID()));
+        try {
+            pdbController.deleteQuery(TableType.FOODREQUESTS, "serviceID", food.getServiceID());
+            pdbController.deleteQuery(TableType.FOODREQUESTS, "serviceID", food2.getServiceID());
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void getAllFoodServiceRequestEntry() {
+        var locName0 = ThreadLocalRandom.current().nextLong();
+        var staff0 = ThreadLocalRandom.current().nextLong();
+        var locName1 = ThreadLocalRandom.current().nextLong();
+        var staff1 = ThreadLocalRandom.current().nextLong();
+        var locName2 = ThreadLocalRandom.current().nextLong();
+        var staff2 = ThreadLocalRandom.current().nextLong();
+        var additionalItem = "testItems";
+        var values0 = new Object[]{UUID.randomUUID(), locName0, staff0, "testNotes", RequestEntry.Status.PROCESSING, "testFood", "testTemp", List.of(additionalItem), "juice", "testRestrictions", "testPatient"};
+        var values1 = new Object[]{UUID.randomUUID(), locName1, staff1, "testNotes", RequestEntry.Status.PROCESSING, "testFood", "testTemp", List.of(additionalItem), "juice", "testRestrictions", "testPatient"};
+        var values2 = new Object[]{UUID.randomUUID(), locName2, staff2, "testNotes", RequestEntry.Status.PROCESSING, "testFood", "testTemp", List.of(additionalItem), "juice", "testRestrictions", "testPatient"};
+        var valuesSet = new Object[][]{values0, values1, values2};
+        var refMap = new HashMap<java.util.UUID, FoodServiceRequestEntry>();
+
+        for (var values : valuesSet) {
+            try {
+                pdbController.insertQuery(TableType.FOODREQUESTS, foodServiceFields, values);
+            } catch (PdbController.DatabaseException e) {
+                throw new RuntimeException(e);
+            }
+            FoodServiceRequestEntry req = new FoodServiceRequestEntry(
+                    (java.util.UUID) values[0],
+                    (java.lang.Long) values[1],
+                    (java.lang.Long) values[2],
+                    (java.lang.String) values[3],
+                    (RequestEntry.Status) values[4],
+                    (java.lang.String) values[5],
+                    (java.lang.String) values[6],
+                    (List<String>) values[7],
+                    (java.lang.String) values[8],
+                    (java.lang.String) values[9],
+                    (java.lang.String) values[10]);
+            refMap.put(req.getServiceID(), req);
+        }
+
+        Map<UUID, FoodServiceRequestEntry> resultMap = facade.getAllFoodServiceRequestEntry();
+
+        for (var key : resultMap.keySet()) {
+            try {
+                pdbController.deleteQuery(TableType.FOODREQUESTS, "serviceID", key);
+            } catch (PdbController.DatabaseException e) {
+                assert false : e.getMessage();
+            }
+        }
+
+        assertEquals(refMap, resultMap);
     }
 
     @Test
     void saveFoodServiceRequestEntry() {
+        UUID uuid = UUID.randomUUID();
+        List<String> additionalItems = new ArrayList<>();
+        additionalItems.add("testItems");
+        var locName = ThreadLocalRandom.current().nextLong();
+        var staff = ThreadLocalRandom.current().nextLong();
+        FoodServiceRequestEntry fsre = new FoodServiceRequestEntry(uuid, locName, staff, "testNotes", RequestEntry.Status.PROCESSING, "testFood", "testTemp", additionalItems, "juice", "testRestrictions", "testPatient");
+        facade.saveFoodServiceRequestEntry(fsre);
+
+        Optional<FoodServiceRequestEntry> results = facade.getFoodServiceRequestEntry(uuid);
+        FoodServiceRequestEntry daoresult = results.get();
+        try {
+            pdbController.deleteQuery(TableType.FOODREQUESTS, "serviceID", uuid);
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(fsre, daoresult);
     }
 
     @Test
     void updateFoodServiceRequestEntry() {
+        UUID uuid = UUID.randomUUID();
+        FoodServiceRequestEntry foodRequest = new FoodServiceRequestEntry(
+                uuid,
+                100L,
+                100L,
+                "testNode0",
+                RequestEntry.Status.PROCESSING,
+                "testFood0",
+                "100",
+                List.of("item1", "item2"),
+                "Juice",
+                "restrictions0",
+                "patientName0"
+        );
+        FoodServiceRequestEntry updateFoodRequest = new FoodServiceRequestEntry(
+                uuid,
+                100L,
+                100L,
+                "testNode0",
+                RequestEntry.Status.PROCESSING,
+                "testFood0",
+                "100",
+                List.of("item1", "item2"),
+                "Apple Juice",
+                "restrictions0",
+                "patientName0"
+        );
+        try {
+            pdbController.insertQuery(TableType.FOODREQUESTS, foodServiceFields, new Object[]{
+                    foodRequest.getServiceID(),
+                    foodRequest.getLocationName(),
+                    foodRequest.getStaffAssignment(),
+                    foodRequest.getAdditionalNotes(),
+                    foodRequest.getStatus(),
+                    foodRequest.getFoodSelection(),
+                    foodRequest.getTempType(),
+                    foodRequest.getAdditionalItems(),
+                    foodRequest.getBeverage(),
+                    foodRequest.getDietaryRestrictions(),
+                    foodRequest.getPatientName()
+            });
+        } catch (PdbController.DatabaseException e) {
+            assert false : e.getMessage();
+        }
+        FoodServiceRequestEntry.Field[] updateFields = {
+                FoodServiceRequestEntry.Field.BEVERAGE
+        };
+        facade.updateFoodServiceRequestEntry(updateFoodRequest, updateFields);
+        Optional<FoodServiceRequestEntry> fsrq = facade.getFoodServiceRequestEntry(uuid);
+        FoodServiceRequestEntry daoresult = fsrq.get();
+        assertEquals(daoresult, updateFoodRequest);
+        try {
+            pdbController.deleteQuery(TableType.FOODREQUESTS, "serviceID", uuid);
+        } catch (PdbController.DatabaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void deleteFoodServiceRequestEntry() {
+        FoodServiceRequestEntry foodRequest = new FoodServiceRequestEntry(
+                UUID.randomUUID(),
+                ThreadLocalRandom.current().nextLong(),
+                ThreadLocalRandom.current().nextLong(),
+                "testNode",
+                RequestEntry.Status.PROCESSING,
+                "testFood",
+                "100",
+                List.of("item1", "item2"),
+                "juice",
+                "restrictions",
+                "patientName"
+        );
+
+
+        var values = new Object[]{
+                foodRequest.getServiceID(),
+                foodRequest.getLocationName(),
+                foodRequest.getStaffAssignment(),
+                foodRequest.getAdditionalNotes(),
+                foodRequest.getStatus(),
+                foodRequest.getFoodSelection(),
+                foodRequest.getTempType(),
+                foodRequest.getAdditionalItems(),
+                foodRequest.getBeverage(),
+                foodRequest.getDietaryRestrictions(),
+                foodRequest.getPatientName()
+        };
+        try {
+            pdbController.insertQuery(TableType.FOODREQUESTS, foodServiceFields, values);
+        } catch (PdbController.DatabaseException e) {
+            assert false : "Failed to insert test data";
+        }
+
+        try {
+            pdbController.searchQuery(TableType.FOODREQUESTS, "serviceID", foodRequest.getServiceID());
+        } catch (PdbController.DatabaseException e) {
+            assert false : "Failed to find test data";
+        }
+
+        facade.deleteFoodServiceRequestEntry(foodRequest);
+
+        try {
+            pdbController.searchQuery(TableType.FOODREQUESTS, "serviceID", foodRequest.getServiceID());
+        } catch (PdbController.DatabaseException e) {
+            assert true : "Successfully deleted test data";
+        }
     }
 
     @Test
     void getFlowerDeliveryRequestEntry() {
+
     }
 
     @Test
