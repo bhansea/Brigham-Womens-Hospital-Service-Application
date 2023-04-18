83fb211342fb8f85256afb26a615a4ce76305b73
Arnav Mishra <49102795+arnavm21@users.noreply.github.com>
2023-04-17 20:57:33 -0400

    Updated Palgo to include DFS
    DFS implemented in Palgo, same thing as BFS but with a stack instead of a queue

--- src/main/java/edu/wpi/punchy_pegasi/backend/pathfinding/Palgo.java
+++ src/main/java/edu/wpi/punchy_pegasi/backend/pathfinding/Palgo.java
@@ -17,6 +17,30 @@ public class Palgo<K, T extends INode> {
         this.targetScorer = targetScorer;
     }
 
+    public List<T> DFS(T from, T to) throws IllegalStateException {
+        Stack<RouteNode<T>> stack = new Stack<>();
+        Map<T, RouteNode<T>> allNodes = new HashMap<>();
+
+        var start = new RouteNode<>(from);
+        stack.push(start);
+        allNodes.put(from, start);
+
+        while (!stack.isEmpty()) {
+            var current = stack.pop();
+            for (var adjNode : graph.getConnections(current.getCurrent())) {
+                if (allNodes.containsKey(adjNode))
+                    continue;
+                var nextNode = new RouteNode<>(adjNode);
+                nextNode.setPrevious(current.getCurrent());
+                allNodes.put(adjNode, nextNode);
+                if (nextNode.getCurrent().equals(to))
+                    return genRoute(allNodes, nextNode);
+                stack.push(nextNode);
+            }
+        }
+        throw new IllegalStateException("No route found");
+    }
+
 
     public List<T> BFS(T from, T to) throws IllegalStateException {
         Queue<RouteNode<T>> queue = new LinkedList<>();
