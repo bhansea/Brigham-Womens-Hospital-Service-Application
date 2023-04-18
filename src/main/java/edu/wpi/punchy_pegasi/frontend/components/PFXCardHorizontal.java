b1674a1b86b10f9ade2aef86260ba04b1f712297
Coops530 <cooper530@users.noreply.github.com>
2023-04-17 20:37:32 -0400

    Continued work on styling for horizontal card.

--- src/main/java/edu/wpi/punchy_pegasi/frontend/components/PFXCardHorizontal.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/components/PFXCardHorizontal.java
@@ -9,44 +9,39 @@ import javafx.scene.layout.VBox;
 public class PFXCardHorizontal extends HBox{
     private final VBox leftPane = new VBox();
     private final VBox rightPane = new VBox();
-    private final VBox imageBox = new VBox();
     private final ImageView image = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/mealPicture.jpeg");
-    private final HBox stats = new HBox();
-    private final VBox title = new VBox();
-    private final VBox info = new VBox();
-    private final VBox selection = new VBox();
     private final PFXPicker picker = new PFXPicker();
+    private final HBox selection = new HBox();
+    private final VBox info = new VBox();
+    private final HBox spacer = new HBox();
+    private final Label titleLabel;
+    private final Label subtitleLabel;
+    private final Label quantity;
 
     public PFXCardHorizontal(){
         super();
+        image.setFitHeight(400);
+        image.setFitWidth(350);
+        titleLabel = new Label("Meal Name");
+        subtitleLabel = new Label("A very nice meal");
+        quantity = new Label("5 Available");
+        rightPane.getChildren().addAll(info, spacer, selection);
+
         getStyleClass().add("pfx-card-horizontal-container");
         getChildren().addAll(leftPane, rightPane);
-        leftPane.getStyleClass().add("pfx-card-horizontal-container");
-        rightPane.getStyleClass().add("pfx-card-horizontal-container");
-        leftPane.getChildren().addAll(imageBox, stats);
-        rightPane.getChildren().add(info);
-        imageBox.getStyleClass().add("pfx-card-horizontal-imageBox");
-        imageBox.getChildren().add(image);
-        HBox.setHgrow(stats, Priority.ALWAYS);
 
-        stats.getStyleClass().add("pfx-card-horizontal-stats");
-        HBox.setHgrow(title, Priority.ALWAYS);
-        HBox.setHgrow(selection, Priority.ALWAYS);
-        stats.getChildren().addAll(title, selection);
+        info.getChildren().addAll(titleLabel, subtitleLabel, selection);
 
-        title.getStyleClass().add("pfx-card-horizontal-info");
+        leftPane.getStyleClass().add("pfx-card-horizontal-imageBox");
+        rightPane.getStyleClass().add("pfx-card-horizontal-stats");
+        leftPane.getChildren().add(image);
+
         selection.getStyleClass().add("pfx-card-horizontal-selection");
-
-        var titleLabel = new Label("Meal Name");
-        title.getChildren().add(titleLabel);
-
-        info.getStyleClass().add("pfx-card-horizontal-info");
-        HBox.setHgrow(info, Priority.ALWAYS);
-        var itemInfo = new Label("A very nice meal. Use the + or - buttons to choose the quantity you want");
-        info.getChildren().add(itemInfo);
-
-
-        var quantity = new Label("1");
         selection.getChildren().addAll(quantity, picker);
+
+        VBox.setVgrow(spacer, Priority.ALWAYS);
+        HBox.setHgrow(leftPane, Priority.ALWAYS);
+        HBox.setHgrow(rightPane, Priority.ALWAYS);
+
     }
 }
