d4b0a7bc048e8e3251b99857985ffb06a3a31d96
Coops530 <cooper530@users.noreply.github.com>
2023-04-18 22:19:24 -0400

    Completed office supplies service request.

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/OfficeServiceRequestController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/OfficeServiceRequestController.java
@@ -3,16 +3,21 @@ package edu.wpi.punchy_pegasi.frontend.controllers.requests;
 
 import edu.wpi.punchy_pegasi.App;
 import edu.wpi.punchy_pegasi.frontend.Screen;
+import edu.wpi.punchy_pegasi.frontend.components.PFXCardHolder;
 import edu.wpi.punchy_pegasi.schema.OfficeServiceRequestEntry;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import edu.wpi.punchy_pegasi.frontend.components.PFXCardVertical;
 import edu.wpi.punchy_pegasi.frontend.components.PFXCardHorizontal;
+import javafx.scene.layout.VBox;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class OfficeServiceRequestController extends RequestController<OfficeServiceRequestEntry> implements PropertyChangeListener {
@@ -25,9 +30,9 @@ public class OfficeServiceRequestController extends RequestController<OfficeServ
     @FXML
     private PFXCardVertical stapler = new PFXCardVertical();
     @FXML
-    private PFXCardVertical staples = new PFXCardVertical();
+    PFXCardHolder cardHolder;
     @FXML
-    private PFXCardVertical paperclips = new PFXCardVertical();
+    VBox container = new VBox();
 
     ArrayList<PFXCardVertical> items = new ArrayList<>();
 
@@ -39,61 +44,33 @@ public class OfficeServiceRequestController extends RequestController<OfficeServ
     @Override
     public void init() {
         submit.setDisable(true);
-        ImageView pencilPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSuppliesPic.jpg");
-        Label pencilLabel = new Label("Pencils");
-        Label pencilInfo = new Label("A thing made of wood and graphite");
-        Label pencilQuantity = new Label("50");
-        pencils = new PFXCardVertical(pencilLabel, pencilInfo, pencilQuantity, pencilPic.getImage());
+        ImageView pencilPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSupplies/colored-pencils.jpg");
+        pencils = new PFXCardVertical("Pencils", "wood and graphite", 50, pencilPic.getImage());
 
-        ImageView penPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSuppliesPic.jpg");
-        Label penLabel = new Label("Pens");
-        Label penInfo = new Label("A thing that has ink.");
-        Label penQuantity = new Label("30");
-        pens = new PFXCardVertical(penLabel, penInfo, penQuantity, penPic.getImage());
+        ImageView penPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSupplies/pen.jpg");
+        pens = new PFXCardVertical("Pens", "It has ink", 30, penPic.getImage());
 
-        ImageView paperPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSuppliesPic.jpg");
-        Label paperLabel = new Label("Paper");
-        Label paperInfo = new Label("A thing made of wood that's really thin and soft");
-        Label paperQuantity = new Label("100");
-        paper = new PFXCardVertical(paperLabel, paperInfo, paperQuantity, paperPic.getImage());
+        ImageView paperPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSupplies/paper.jpg");
+        paper = new PFXCardVertical("Paper", "Thin and soft", 100, paperPic.getImage());
 
-        ImageView staplerPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSuppliesPic.jpg");
-        Label staplerLabel = new Label("Paper");
-        Label staplerInfo = new Label("A thing made of wood that's really thin and soft");
-        Label staplerQuantity = new Label("100");
-        stapler = new PFXCardVertical(staplerLabel, staplerInfo, staplerQuantity, staplerPic.getImage());
+        ImageView staplerPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSupplies/stapler.jpg");
+        stapler = new PFXCardVertical("Stapler", "Staples papers together", 5, staplerPic.getImage());
 
-        ImageView staplesPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSuppliesPic.jpg");
-        Label staplesLabel = new Label("Paper");
-        Label staplesInfo = new Label("A thing made of wood that's really thin and soft");
-        Label staplesQuantity = new Label("100");
-        staples = new PFXCardVertical(staplesLabel, staplesInfo, staplesQuantity, staplesPic.getImage());
+        cardHolder = new PFXCardHolder(new ArrayList<>(Arrays.asList(pencils, pens, paper, stapler)));
 
-        ImageView paperclipsPic = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/officeSuppliesPic.jpg");
-        Label paperclipsLabel = new Label("Paper");
-        Label paperclipsInfo = new Label("A thing made of wood that's really thin and soft");
-        Label paperclipsQuantity = new Label("100");
-        paperclips = new PFXCardVertical(paperclipsLabel, paperclipsInfo, paperclipsQuantity, paperclipsPic.getImage());
-
-        items.add(pencils);
-        items.add(pens);
-        items.add(paper);
-        items.add(stapler);
-        items.add(staples);
-        items.add(paperclips);
+        container.getChildren().add(cardHolder);
+        submit.setDisable(true);
+        this.addPropertyChangeListener(this);
+        container.setAlignment(Pos.CENTER);
         setHeaderText("Office Supplies Service Request");
 }
 
     @FXML
     public void submitEntry() {
-//        StringBuilder reqString = new StringBuilder();
-//        for (int i = 0; i < items.size(); i++)
-//            reqString.append(items.get(i).getTitle()).append(" - ").append(items.get(i).getQuantity()).append("; ");
-
         //makes sure shared fields aren't empty
-        //requestEntry = new OfficeServiceRequestEntry(locationName.getSelectedItem().getUuid(), staffAssignment.getSelectedItem().getEmployeeID(), additionalNotes.getText(), reqString.toString().trim(), "");
-        //App.getSingleton().getFacade().saveOfficeServiceRequestEntry(requestEntry);
-        //App.getSingleton().navigate(Screen.HOME);
+        requestEntry = new OfficeServiceRequestEntry(locationName.getSelectedItem().getUuid(), staffAssignment.getSelectedItem().getEmployeeID(), additionalNotes.getText(), cardHolder.getChosenItems(), 1L);
+        App.getSingleton().getFacade().saveOfficeServiceRequestEntry(requestEntry);
+        App.getSingleton().navigate(Screen.HOME);
     }
 
     @Override
@@ -103,17 +80,14 @@ public class OfficeServiceRequestController extends RequestController<OfficeServ
 
     @FXML
     public void validateEntry() {
-//        var validate = validateGeneric() || items.stream().filter(CheckBox::isSelected).toList().size() == 0;
-//        for (int i = 0; i < items.size(); i++)
-//            validate |= itemsAmount.get(i).getText().isBlank() && items.get(i).isSelected();
-//        submit.setDisable(validate);
-        submit.setDisable(false);
+        var validate = validateGeneric();
+        submit.setDisable(validate);
     }
 
     @FXML
     public void clearEntry() {
         clearGeneric();
-
+        cardHolder.clear();
     }
 
     @Override
