94ca0d842170bb44deff255a611188eaef9874f9
tommywwz <wangweizhe2001@gmail.com>
2023-04-22 00:39:54 -0400

    add theme switch and full screen on signage

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SignageController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SignageController.java
@@ -3,23 +3,41 @@ package edu.wpi.punchy_pegasi.frontend.controllers;
 import edu.wpi.punchy_pegasi.App;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 
 public class SignageController {
+
+    private final String lightTheme = getClass().getResource("/edu/wpi/punchy_pegasi/frontend/css/SignageLight.css").toExternalForm();
+    private final String darkTheme = getClass().getResource("/edu/wpi/punchy_pegasi/frontend/css/SignageDark.css").toExternalForm();
+
+    private final Scene myScene = App.getSingleton().getScene();
     @FXML
     private void initialize() {
         Platform.runLater(() -> {
+            switchTheme(true);
             App.getSingleton().getPrimaryStage().setFullScreen(true);
-//            App.getSingleton().getLayout().showLeftLayout(false);
-//            App.getSingleton().getLayout().showTopLayout(false);
+            App.getSingleton().getLayout().showLeftLayout(false);
+            App.getSingleton().getLayout().showTopLayout(false);
         });
-        App.getSingleton().getScene().setOnKeyTyped(event -> {
-            if(event.getCode() == KeyCode.ESCAPE) {
+        myScene.setOnKeyPressed(event -> {
+            if(event.getCode().equals(KeyCode.ESCAPE)) {
+                switchTheme(false);
                 App.getSingleton().getPrimaryStage().setFullScreen(false);
-//                App.getSingleton().getLayout().showLeftLayout(true);
-//                App.getSingleton().getLayout().showTopLayout(true);
+                App.getSingleton().getLayout().showLeftLayout(true);
+                App.getSingleton().getLayout().showTopLayout(true);
             }
         });
+    }
+
+    private void switchTheme(boolean setDark) {
+        if (setDark) {
+            myScene.getStylesheets().remove(lightTheme);
+            myScene.getStylesheets().add(darkTheme);
+        } else {
+            myScene.getStylesheets().remove(darkTheme);
+            myScene.getStylesheets().add(lightTheme);
+        }
     }
 
 }
