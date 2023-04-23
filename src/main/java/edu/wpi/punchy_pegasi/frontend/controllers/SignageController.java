ae4145298f1f13c414193ba3021e91e4cb56fb04
tommywwz <wangweizhe2001@gmail.com>
2023-04-23 00:10:13 -0400

    connect signage location name to db

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SignageController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SignageController.java
@@ -3,13 +3,14 @@ package edu.wpi.punchy_pegasi.frontend.controllers;
 import edu.wpi.punchy_pegasi.App;
 import edu.wpi.punchy_pegasi.frontend.icons.PFXIcon;
 import edu.wpi.punchy_pegasi.generated.Facade;
+import edu.wpi.punchy_pegasi.schema.Signage;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 
-import java.util.Objects;
+import java.util.*;
 
 public class SignageController {
 
@@ -20,12 +21,40 @@ public class SignageController {
     private final Facade facade = App.getSingleton().getFacade();
 
     @FXML
-    private Label labelFWD;
+    private Label labelUP;
+    @FXML
+    private Label labelDOWN;
+    @FXML
+    private Label labelLFT;
+    @FXML
+    private Label labelRGT;
+    @FXML
+    private Label labelHERE;
+
+    @FXML
+    private PFXIcon iconFWD;
+    @FXML
+    private PFXIcon iconAFT;
+    @FXML
+    private PFXIcon iconLFT;
+    @FXML
+    private PFXIcon iconRGT;
+    @FXML
+    private PFXIcon iconHERE;
 
     @FXML
     private void initialize() {
-        String textFWD = "";
-        labelFWD.setText(textFWD);
+        Signage newSignageD = new Signage("Location Name Down 0", Signage.DirectionType.DOWN);
+        Signage newSignageU = new Signage("Location Name Up 0", Signage.DirectionType.UP);
+        Signage newSignageD1 = new Signage("Location Name Down 1", Signage.DirectionType.DOWN);
+        facade.saveSignage(newSignageD);
+        facade.saveSignage(newSignageU);
+        facade.saveSignage(newSignageD1);
+        updateSignage();
+
+        facade.deleteSignage(newSignageD);
+        facade.deleteSignage(newSignageU);
+        facade.deleteSignage(newSignageD1);
         Platform.runLater(() -> {
             switchTheme(true);
             App.getSingleton().getPrimaryStage().setFullScreen(true);
@@ -42,8 +71,38 @@ public class SignageController {
         });
     }
 
-    @FXML
-
+    private void updateSignage() {
+        Map<String, Signage> allSignage =  facade.getAllSignage();
+        StringBuilder sbUp = new StringBuilder();
+        StringBuilder sbDown = new StringBuilder();
+        StringBuilder sbLeft = new StringBuilder();
+        StringBuilder sbRight = new StringBuilder();
+        StringBuilder sbHere = new StringBuilder();
+        for (Map.Entry<String, Signage> entry : allSignage.entrySet()) {
+            switch (entry.getValue().getDirectionType()) {
+                case UP -> {
+                    sbUp.append(entry.getKey()).append("\n");
+                }
+                case DOWN -> {
+                    sbDown.append(entry.getKey()).append("\n");
+                }
+                case LEFT -> {
+                    sbLeft.append(entry.getKey()).append("\n");
+                }
+                case RIGHT -> {
+                    sbRight.append(entry.getKey()).append("\n");
+                }
+                case HERE -> {
+                    sbHere.append(entry.getKey()).append("\n");
+                }
+            }
+        }
+        labelUP.setText(sbUp.toString());
+        labelDOWN.setText(sbDown.toString());
+        labelLFT.setText(sbLeft.toString());
+        labelRGT.setText(sbRight.toString());
+        labelHERE.setText(sbHere.toString());
+    }
 
     private void switchTheme(boolean setDark) {
         if (setDark) {
