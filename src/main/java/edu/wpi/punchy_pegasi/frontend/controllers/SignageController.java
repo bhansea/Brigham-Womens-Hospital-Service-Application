815d0fb0134dd1310eaafb89792b4625f985a275
tommywwz <wangweizhe2001@gmail.com>
2023-04-23 01:57:11 -0400

    rebuild fxml in signage controller

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SignageController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SignageController.java
@@ -1,6 +1,7 @@
 package edu.wpi.punchy_pegasi.frontend.controllers;
 
 import edu.wpi.punchy_pegasi.App;
+import edu.wpi.punchy_pegasi.frontend.icons.MaterialSymbols;
 import edu.wpi.punchy_pegasi.frontend.icons.PFXIcon;
 import edu.wpi.punchy_pegasi.generated.Facade;
 import edu.wpi.punchy_pegasi.schema.Signage;
@@ -9,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.*;
 
 import java.util.*;
 
@@ -20,41 +22,58 @@ public class SignageController {
 
     private final Facade facade = App.getSingleton().getFacade();
 
-    @FXML
-    private Label labelUP;
-    @FXML
-    private Label labelDOWN;
-    @FXML
-    private Label labelLFT;
-    @FXML
-    private Label labelRGT;
+//    @FXML
+//    private Label labelUP;
+//    @FXML
+//    private Label labelDOWN;
+//    @FXML
+//    private Label labelLFT;
+//    @FXML
+//    private Label labelRGT;
     @FXML
     private Label labelHERE;
 
     @FXML
-    private PFXIcon iconFWD;
+    private VBox signageBodyLeft;
+//    @FXML
+//    private HBox signageUpHB;
+//    @FXML
+//    private HBox signageDownHB;
+//    @FXML
+//    private HBox signageLeftHB;
+//    @FXML
+//    private HBox signageRightHB;
     @FXML
-    private PFXIcon iconAFT;
-    @FXML
-    private PFXIcon iconLFT;
-    @FXML
-    private PFXIcon iconRGT;
+    private HBox signageHereHB;
+
+
+    private PFXIcon iconUp = new PFXIcon(MaterialSymbols.ARROW_UPWARD);
+
+    private PFXIcon iconDown = new PFXIcon(MaterialSymbols.ARROW_DOWNWARD);
+
+    private PFXIcon iconLeft = new PFXIcon(MaterialSymbols.ARROW_BACK);
+
+    private PFXIcon iconRight = new PFXIcon(MaterialSymbols.ARROW_FORWARD);
+
+
     @FXML
     private PFXIcon iconHERE;
 
     @FXML
     private void initialize() {
+        initIcons();
         Signage newSignageD = new Signage("Location Name Down 0", Signage.DirectionType.DOWN);
         Signage newSignageU = new Signage("Location Name Up 0", Signage.DirectionType.UP);
         Signage newSignageD1 = new Signage("Location Name Down 1", Signage.DirectionType.DOWN);
         facade.saveSignage(newSignageD);
         facade.saveSignage(newSignageU);
         facade.saveSignage(newSignageD1);
-        updateSignage();
+        buildSignage(loadSignage());
 
         facade.deleteSignage(newSignageD);
         facade.deleteSignage(newSignageU);
         facade.deleteSignage(newSignageD1);
+
         Platform.runLater(() -> {
             switchTheme(true);
             App.getSingleton().getPrimaryStage().setFullScreen(true);
@@ -71,37 +90,79 @@ public class SignageController {
         });
     }
 
-    private void updateSignage() {
+    private void initIcons () {
+        iconUp.setOutlined(true);
+        iconUp.getStyleClass().add("signage-icon");
+        iconDown.setOutlined(true);
+        iconDown.getStyleClass().add("signage-icon");
+        iconLeft.setOutlined(true);
+        iconLeft.getStyleClass().add("signage-icon");
+        iconRight.setOutlined(true);
+        iconRight.getStyleClass().add("signage-icon");
+    }
+
+    private Map<Signage.DirectionType, List<String>> loadSignage() {
+        Map<Signage.DirectionType, List<String>> signageMap = new HashMap<>();
         Map<String, Signage> allSignage =  facade.getAllSignage();
-        StringBuilder sbUp = new StringBuilder();
-        StringBuilder sbDown = new StringBuilder();
-        StringBuilder sbLeft = new StringBuilder();
-        StringBuilder sbRight = new StringBuilder();
-        StringBuilder sbHere = new StringBuilder();
+//        StringBuilder sbUp = new StringBuilder();
+//        StringBuilder sbDown = new StringBuilder();
+//        StringBuilder sbLeft = new StringBuilder();
+//        StringBuilder sbRight = new StringBuilder();
+//        StringBuilder sbHere = new StringBuilder();
         for (Map.Entry<String, Signage> entry : allSignage.entrySet()) {
-            switch (entry.getValue().getDirectionType()) {
+            Signage.DirectionType directionType = entry.getValue().getDirectionType();
+            switch (directionType) {
                 case UP -> {
-                    sbUp.append(entry.getKey()).append("\n");
+                    signageMap.computeIfAbsent(Signage.DirectionType.UP, k -> new ArrayList<>()).add(entry.getKey());
                 }
                 case DOWN -> {
-                    sbDown.append(entry.getKey()).append("\n");
+                    signageMap.computeIfAbsent(Signage.DirectionType.DOWN, k -> new ArrayList<>()).add(entry.getKey());
                 }
                 case LEFT -> {
-                    sbLeft.append(entry.getKey()).append("\n");
+                    signageMap.computeIfAbsent(Signage.DirectionType.LEFT, k -> new ArrayList<>()).add(entry.getKey());
                 }
                 case RIGHT -> {
-                    sbRight.append(entry.getKey()).append("\n");
+                    signageMap.computeIfAbsent(Signage.DirectionType.RIGHT, k -> new ArrayList<>()).add(entry.getKey());
                 }
                 case HERE -> {
-                    sbHere.append(entry.getKey()).append("\n");
+                    signageMap.computeIfAbsent(Signage.DirectionType.HERE, k -> new ArrayList<>()).add(entry.getKey());
                 }
             }
         }
-        labelUP.setText(sbUp.toString());
-        labelDOWN.setText(sbDown.toString());
-        labelLFT.setText(sbLeft.toString());
-        labelRGT.setText(sbRight.toString());
-        labelHERE.setText(sbHere.toString());
+        return signageMap;
+    }
+
+    private void buildSignage(Map<Signage.DirectionType, List<String>> signageMap) {
+        for (Map.Entry<Signage.DirectionType, List<String>> entry : signageMap.entrySet()) {
+            Label locLabel = new Label(String.join("\n", entry.getValue()));
+            locLabel.getStyleClass().add("signage-label");
+            HBox signageHB = new HBox();
+            signageHB.getStyleClass().add("signage-list");
+            switch (entry.getKey()) {
+                case UP -> {
+                    signageHB.getChildren().add(iconUp);
+                    signageHB.getChildren().add(locLabel);
+                }
+                case DOWN -> {
+                    signageHB.getChildren().add(iconDown);
+                    signageHB.getChildren().add(locLabel);
+                }
+                case LEFT -> {
+                    signageHB.getChildren().add(iconLeft);
+                    signageHB.getChildren().add(locLabel);
+                }
+                case RIGHT -> {
+                    signageHB.getChildren().add(iconRight);
+                    signageHB.getChildren().add(locLabel);
+                }
+                case HERE -> {
+                    signageHB.getChildren().add(iconHERE);
+                    signageHB.getChildren().add(locLabel);
+                }
+            }
+            signageBodyLeft.getChildren().add(signageHB);
+            signageBodyLeft.getStyleClass().add("signage-body-left");
+        }
     }
 
     private void switchTheme(boolean setDark) {
