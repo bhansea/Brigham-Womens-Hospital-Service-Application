27f2111d6ac06f8a97b80e2ff8090d8fefedde15
bhansea <97768606+bhansea@users.noreply.github.com>
2023-04-23 13:52:18 -0400

    added About page to sidebar displayed some text on the About page

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SidebarController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SidebarController.java
@@ -28,7 +28,8 @@ public class SidebarController extends VBox implements PropertyChangeListener {
             new PFXSidebarItem(Screen.MAP_PAGE, MaterialSymbols.MAP),
             new PFXSidebarItem(Screen.ADMIN_PAGE, MaterialSymbols.ADMIN_PANEL_SETTINGS),
             new PFXSidebarItem(Screen.EDIT_MAP_PAGE, MaterialSymbols.REBASE_EDIT),
-            new PFXSidebarItem(Screen.SIGNAGE, MaterialSymbols.SIGNPOST)
+            new PFXSidebarItem(Screen.SIGNAGE, MaterialSymbols.SIGNPOST),
+            new PFXSidebarItem(Screen.ABOUT, MaterialSymbols.QUESTION_MARK)
     );
     private final int maxWidth = 256;
     @FXML
