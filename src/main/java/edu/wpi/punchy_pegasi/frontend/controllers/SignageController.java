4d18008516a3e09d30e1639d7244d924c6d2da58
tommywwz <wangweizhe2001@gmail.com>
2023-04-22 22:37:37 -0400

    made fx id for label

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SignageController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/SignageController.java
@@ -1,19 +1,31 @@
 package edu.wpi.punchy_pegasi.frontend.controllers;
 
 import edu.wpi.punchy_pegasi.App;
+import edu.wpi.punchy_pegasi.frontend.icons.PFXIcon;
+import edu.wpi.punchy_pegasi.generated.Facade;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
+
+import java.util.Objects;
 
 public class SignageController {
 
-    private final String lightTheme = getClass().getResource("/edu/wpi/punchy_pegasi/frontend/css/SignageLight.css").toExternalForm();
-    private final String darkTheme = getClass().getResource("/edu/wpi/punchy_pegasi/frontend/css/SignageDark.css").toExternalForm();
-
+    private final String lightTheme = Objects.requireNonNull(getClass().getResource("/edu/wpi/punchy_pegasi/frontend/css/SignageLight.css")).toExternalForm();
+    private final String darkTheme = Objects.requireNonNull(getClass().getResource("/edu/wpi/punchy_pegasi/frontend/css/SignageDark.css")).toExternalForm();
     private final Scene myScene = App.getSingleton().getScene();
+
+    private final Facade facade = App.getSingleton().getFacade();
+
+    @FXML
+    private Label labelFWD;
+
     @FXML
     private void initialize() {
+        String textFWD = "";
+        labelFWD.setText(textFWD);
         Platform.runLater(() -> {
             switchTheme(true);
             App.getSingleton().getPrimaryStage().setFullScreen(true);
@@ -29,6 +41,9 @@ public class SignageController {
             }
         });
     }
+
+    @FXML
+
 
     private void switchTheme(boolean setDark) {
         if (setDark) {
