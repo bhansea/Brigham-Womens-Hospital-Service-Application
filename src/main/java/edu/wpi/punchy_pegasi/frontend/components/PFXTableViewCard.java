931447e73f2a4aa8079d35a5c0e8d6f4d358cf56
charcar515 <106764357+charcar515@users.noreply.github.com>
2023-04-23 14:52:40 -0400

    created class to see if image in vertical card is messing things up

--- src/main/java/edu/wpi/punchy_pegasi/frontend/components/PFXTableViewCard.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/components/PFXTableViewCard.java
@@ -1,48 +1,61 @@
 package edu.wpi.punchy_pegasi.frontend.components;
 
-import edu.wpi.punchy_pegasi.generator.schema.RequestEntry;
-import javafx.scene.control.TableView;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
-import static com.sun.javafx.css.StyleClassSet.getStyleClass;
+public class PFXCardVerticalPlaceholder extends VBox {
+    private final VBox imageBox = new VBox();
+    // private final ImageView image;
+    private final HBox stats = new HBox();
+    private final VBox info = new VBox();
+    private final VBox selection = new VBox();
+    private final PFXPicker picker = new PFXPicker();
+    // private final Label titleLabel;
+    //private final Label subtitleLabel;
+    //private final Label quantity;
 
-public class PFXTableViewCard {
-    private VBox tableBox = new VBox();
-    private TableView<RequestEntry> tableView = new TableView<>();
-
-    public PFXTableViewCard() {
+    public PFXCardVerticalPlaceholder() {
         super();
+//        image = new ImageView("edu/wpi/punchy_pegasi/frontend/assets/mealPicture.jpeg");
+//        image.setFitHeight(400);
+//        image.setFitWidth(300);
+//        titleLabel = new Label("Meal Name");
+//        subtitleLabel = new Label("A very nice meal");
+//        quantity = new Label("0 Available");
 
-        tableView = new TableView<>();
-        getStyleClass("-pfx-table-card-vertical-container");
+        getStyleClass().add("pfx-card-vertical-container");
+        getChildren().addAll(imageBox, stats);
+        imageBox.getStyleClass().add("pfx-card-vertical-imageBox");
+        //imageBox.getChildren().add(image);
+        HBox.setHgrow(stats, Priority.ALWAYS);
 
-        tableBox.getStyleClass().add("pfx-table-box");
-        tableBox.getChildren().add(tableView);
-    }
-//        getStyleClass().add("pfx-card-vertical-container");
-//        getChildren().addAll(imageBox, stats);
-//        imageBox.getStyleClass().add("pfx-card-vertical-imageBox");
-//        imageBox.getChildren().add(image);
-//        HBox.setHgrow(stats, Priority.ALWAYS);
-//
-//        stats.getStyleClass().add("pfx-card-vertical-stats");
-//        HBox.setHgrow(info, Priority.ALWAYS);
-//        HBox.setHgrow(selection, Priority.ALWAYS);
-//        stats.getChildren().addAll(info, selection);
-//
-//        info.getStyleClass().add("pfx-card-vertical-info");
-//        selection.getStyleClass().add("pfx-card-vertical-selection");
-//
-//
+        stats.getStyleClass().add("pfx-card-vertical-stats");
+        HBox.setHgrow(info, Priority.ALWAYS);
+        HBox.setHgrow(selection, Priority.ALWAYS);
+        stats.getChildren().addAll(info, selection);
+
+        info.getStyleClass().add("pfx-card-vertical-info");
+        selection.getStyleClass().add("pfx-card-vertical-selection");
+
+
 //        info.getChildren().addAll(titleLabel, subtitleLabel);
-//        selection.getChildren().addAll(quantity, picker);
+        selection.getChildren().addAll(picker);
     }
+}
 
-//    public PFXTableViewCard(String title, String subtitle, int quantity, Image image) {
+//    public PFXCardVerticalPlaceholder(String title, String subtitle, int quantity, Image image) {
 //        super();
-//        this.tableBox = new VBox();
-//        this.tableView = new TableView<>();
-
+//        this.image = new ImageView(image);
+//        this.image.setFitHeight(300);
+//        this.image.setFitWidth(400);
+//        this.titleLabel = new Label(title);
+//        this.subtitleLabel = new Label(subtitle);
+//        this.quantity = new Label(Integer.toString(quantity));
+//
 //        getStyleClass().add("pfx-card-vertical-container");
 //        getChildren().addAll(imageBox, stats);
 //        imageBox.getStyleClass().add("pfx-card-vertical-imageBox");
@@ -59,7 +72,17 @@ public class PFXTableViewCard {
 //
 //        info.getChildren().addAll(titleLabel, subtitleLabel);
 //        selection.getChildren().addAll(this.quantity, picker);
-
-
-
-
+//    }
+//
+//    public int getQuantity() {
+//        return picker.getQuantity();
+//    }
+//
+//    public String getTitle() {
+//        return titleLabel.getText();
+//    }
+//
+//    public void clearQuantity() {
+//        picker.clearQuantity();
+//    }
+//}
