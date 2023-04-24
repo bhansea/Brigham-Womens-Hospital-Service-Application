cf7b684773020f62771051c9f071717c6adf4bf1
RyanNguyen321 <nguyenryan0903@gmail.com>
2023-04-23 22:52:17 -0400

    New admin page. Better functionality, new ui technically

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/adminPage/AdminTablePageController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/requests/adminPage/AdminTablePageController.java
@@ -60,7 +60,6 @@ public class AdminTablePageController {
     @FXML
     private FlowPane editContainer;
     private AdminTable currentTable;
-
     private ArrayList<TextArea> textAreas = new ArrayList<>();
 
     // import export stuff
@@ -69,7 +68,6 @@ public class AdminTablePageController {
     private File selectedDir = new File("");
     @FXML
     private Label fileText = new Label();
-
     FileChooser fileChooser = new FileChooser();
     DirectoryChooser directoryChooser = new DirectoryChooser();
     PdbController pdb = App.getSingleton().getPdb();
@@ -85,7 +83,6 @@ public class AdminTablePageController {
 
         initTables();
         displayEditComponent();
-
 
         displayTableTypeComboBox.setOnAction(e -> {
             String name = displayTableTypeComboBox.getSelectedItem();
@@ -113,85 +110,56 @@ public class AdminTablePageController {
         });
 
         submitEditButton.setOnAction(e -> {
+            // location name works, additional notes, status, employee ID. anything after general request entries arent recorded, this is hard
             if (currentTable.humanReadableName.toLowerCase().contains("request")) {
-                RequestEntry.Status status = null;
-                    if (textAreas.get(4).getText() == "NONE") {
-                        status = RequestEntry.Status.NONE;
-                    } else if (textAreas.get(4).getText() == "PROCESSING") {
-                        status = RequestEntry.Status.PROCESSING;
-                    } else if (textAreas.get(4).getText() == "DONE") {
-                        status = RequestEntry.Status.DONE;
-                    }
+                RequestEntry.Status status = RequestEntry.Status.valueOf(textAreas.get(4).getText());
                 var requestEntry = new RequestEntry(UUID.fromString(textAreas.get(0).getText()), Long.parseLong(textAreas.get(1).getText()), Long.parseLong(textAreas.get(2).getText()), textAreas.get(3).getText(), status, Long.parseLong(textAreas.get(5).getText()));
                 facade.updateRequestEntry(requestEntry, new RequestEntry.Field[]{RequestEntry.Field.SERVICE_ID, RequestEntry.Field.LOCATION_NAME, RequestEntry.Field.STAFF_ASSIGNMENT, RequestEntry.Field.ADDITIONAL_NOTES, RequestEntry.Field.STATUS, RequestEntry.Field.EMPLOYEE_ID});
                 currentTable.reload();
                 currentTable.table.update();
             }
+            // WORKS, employee id doesn't work
             else if (currentTable.humanReadableName.toLowerCase().contains("employee")) {
                 var employee = new Employee(Long.parseLong(textAreas.get(0).getText()), textAreas.get(1).getText(), textAreas.get(2).getText());
                 facade.updateEmployee(employee, new Employee.Field[]{Employee.Field.EMPLOYEE_ID, Employee.Field.FIRST_NAME, Employee.Field.LAST_NAME});
                 currentTable.reload();
                 currentTable.table.update();
-            } else if (currentTable.humanReadableName.toLowerCase().contains("account")) {
-                Account.AccountType status = null;
-                    if (textAreas.get(4).getText() == "NONE") {
-                        status = Account.AccountType.NONE;
-                    } else if (textAreas.get(4).getText() == "ADMIN") {
-                        status = Account.AccountType.ADMIN;
-                    } else if (textAreas.get(4).getText() == "STAFF") {
-                        status = Account.AccountType.STAFF;
-                    }
-                var account = new Account(Long.parseLong(textAreas.get(0).getText()), textAreas.get(1).getText(),textAreas.get(2).getText(), Long.parseLong(textAreas.get(3).getText()), status);
+            }
+            // works, id doesn't work
+            else if (currentTable.humanReadableName.toLowerCase().contains("account")) {
+                Account.AccountType accountType = Account.AccountType.valueOf(textAreas.get(4).getText());
+                var account = new Account(Long.parseLong(textAreas.get(0).getText()), textAreas.get(1).getText(),textAreas.get(2).getText(), Long.parseLong(textAreas.get(3).getText()), accountType);
                 facade.updateAccount(account, new Account.Field[]{Account.Field.UUID, Account.Field.USERNAME, Account.Field.PASSWORD, Account.Field.EMPLOYEE_ID, Account.Field.ACCOUNT_TYPE});
                 currentTable.reload();
                 currentTable.table.update();
-
-            } else if (currentTable.humanReadableName.toLowerCase().contains("node")) {
+            }
+            // Works? dissapperars from table even on reload, odesnt work with id
+            else if (currentTable.humanReadableName.toLowerCase().contains("node")) {
                 var node = new Node(Long.parseLong(textAreas.get(0).getText()), Integer.parseInt(textAreas.get(1).getText()), Integer.parseInt(textAreas.get(2).getText()), textAreas.get(3).getText(), textAreas.get(4).getText());
                 facade.updateNode(node, new Node.Field[]{Node.Field.NODE_ID, Node.Field.XCOORD, Node.Field.YCOORD, Node.Field.FLOOR, Node.Field.BUILDING});
                 currentTable.reload();
                 currentTable.table.update();
 
-            } else if (currentTable.humanReadableName.toLowerCase().contains("edge")) {
+            }
+            // works, without ID dissappears from table
+            else if (currentTable.humanReadableName.toLowerCase().contains("edge")) {
                 var edge = new Edge(Long.parseLong(textAreas.get(0).getText()), Long.parseLong(textAreas.get(1).getText()), Long.parseLong(textAreas.get(2).getText()));
                 facade.updateEdge(edge, new Edge.Field[]{Edge.Field.UUID, Edge.Field.START_NODE, Edge.Field.END_NODE});
                 currentTable.reload();
                 currentTable.table.update();
 
-            } else if (currentTable.humanReadableName.toLowerCase().contains("move")) {
+            }
+            // works, without idd
+            else if (currentTable.humanReadableName.toLowerCase().contains("move")) {
                 var move = new Move(Long.parseLong(textAreas.get(0).getText()), Long.parseLong(textAreas.get(1).getText()), Long.parseLong(textAreas.get(2).getText()), LocalDate.parse(textAreas.get(3).getText()));
                 facade.updateMove(move, new Move.Field[]{Move.Field.UUID, Move.Field.NODE_ID, Move.Field.LOCATION_ID, Move.Field.DATE});
                 currentTable.reload();
                 currentTable.table.update();
 
-            } else if (currentTable.humanReadableName.toLowerCase().contains("location")) {
-
-                LocationName.NodeType nodeType = null;
-                    if (textAreas.get(3).getText() == "HALL") {
-                        nodeType = LocationName.NodeType.HALL;
-                    } else if (textAreas.get(3).getText() == "ELEV") {
-                        nodeType = LocationName.NodeType.HALL;
-                    } else if (textAreas.get(3).getText() == "REST") {
-                        nodeType = LocationName.NodeType.REST;
-                    } else if (textAreas.get(3).getText() == "STAI") {
-                        nodeType = LocationName.NodeType.STAI;
-                    } else if (textAreas.get(3).getText() == "DEPT") {
-                        nodeType = LocationName.NodeType.DEPT;
-                    } else if (textAreas.get(3).getText() == "LABS") {
-                        nodeType = LocationName.NodeType.LABS;
-                    } else if (textAreas.get(3).getText() == "INFO") {
-                        nodeType = LocationName.NodeType.INFO;
-                    } else if (textAreas.get(3).getText() == "CONF") {
-                        nodeType = LocationName.NodeType.CONF;
-                    } else if (textAreas.get(3).getText() == "EXIT") {
-                        nodeType = LocationName.NodeType.EXIT;
-                    } else if (textAreas.get(3).getText() == "RETL") {
-                        nodeType = LocationName.NodeType.RETL;
-                    } else if (textAreas.get(3).getText() == "SERV") {
-                        nodeType = LocationName.NodeType.SERV;
-                    } else if (textAreas.get(3).getText() == "BATH") {
-                        nodeType = LocationName.NodeType.BATH;
-                    }
+            }
+            // works without ID, dissappears from table
+            else if (currentTable.humanReadableName.toLowerCase().contains("location")) {
+                LocationName.NodeType nodeType = LocationName.NodeType.valueOf(textAreas.get(3).getText());
                 var location = new LocationName(Long.parseLong(textAreas.get(0).getText()), textAreas.get(1).getText(), textAreas.get(2).getText(), nodeType);
                 facade.updateLocationName(location, new LocationName.Field[]{LocationName.Field.UUID, LocationName.Field.LONG_NAME, LocationName.Field.SHORT_NAME, LocationName.Field.NODE_TYPE});
                 currentTable.reload();
@@ -199,37 +167,6 @@ public class AdminTablePageController {
 
             }
         });
-
-//        if (r instanceof RequestEntry) {
-//                    RequestEntry entry = (RequestEntry) r;
-//                    submitRequestEditButton.setOnAction(e -> {
-//                        RequestEntry.Status status = null;
-//                        if (statusType.getSelectedItem() == "NONE") {
-//                            status = RequestEntry.Status.NONE;
-//                        } else if (statusType.getSelectedItem() == "PROCESSING") {
-//                            status = RequestEntry.Status.PROCESSING;
-//                        } else if (statusType.getSelectedItem() == "DONE") {
-//                            status = RequestEntry.Status.DONE;
-//                        }
-//                        if (status == null) return;
-//                        entry.setStatus(status);
-//                        facade.updateRequestEntry(entry, new RequestEntry.Field[]{RequestEntry.Field.STATUS});
-////                        t.reload();
-//                        t.table.update();
-//                    });
-//                } else if (r instanceof Employee) {
-//                    Employee employee = (Employee) r;
-//                    firstNameField.setText(employee.getFirstName());
-//                    lastNameField.setText(employee.getLastName());
-//                    submitEmployeeEditButton.setOnAction(e -> {
-//                        var em = new Employee(((Employee) r).getEmployeeID(), firstNameField.getText(), lastNameField.getText());
-//                        facade.updateEmployee(em, new Employee.Field[]{Employee.Field.FIRST_NAME, Employee.Field.LAST_NAME});
-////                        t.reload();
-//                        t.table.update();
-//                    });
-//                }
-
-
 
         importButton.setOnAction(e -> {
             FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
@@ -266,8 +203,6 @@ public class AdminTablePageController {
 //            pdb.deleteQuery(currentTable, );
         });
     }
-
-
     public void showTable(AdminTable tableType) {
         currentTable = tableType;
         currentTable.getTable().setVisible(true);
@@ -283,7 +218,6 @@ public class AdminTablePageController {
         tableContainer.getChildren().addAll(tables.values().stream().map(AdminTable::getTable).toList());
         showTable(tables.get("Node"));
     }
-
     public void displayEditComponent() {
         HBox hbox = new HBox();
         editContainer.getChildren().clear();
@@ -292,13 +226,10 @@ public class AdminTablePageController {
             TextArea fieldArea = new TextArea();
 
             fieldLabel.setText(field.toString());
-            hbox.getChildren().add(fieldLabel);
-            hbox.getChildren().add(fieldArea);
+            editContainer.getChildren().add(fieldLabel);
+            editContainer.getChildren().add(fieldArea);
         }
         editContainer.getChildren().add(hbox);
         editContainer.getStyleClass().add("admin-edit-container");
     }
-
-
-
 }
