35245f98fde75315ff4c4d8ccc56189c182fe0b8
Arnav Mishra <49102795+arnavm21@users.noreply.github.com>
2023-04-24 23:50:29 -0400

    Fixing formatting
    Fixing gridpane formatting

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/CreditsController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/CreditsController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,6 +23,8 @@ public class CreditsController {
 
     @FXML
     private void initialize(){
+        sourceGrid.getColumnConstraints().add(new ColumnConstraints(150));
+        sourceGrid.getColumnConstraints().add(new ColumnConstraints(150));
         resources.add(new Resource("Google Fonts", "icons", new Hyperlink("https://fonts.google.com/icons")));
         resources.add(new Resource("jSerialComm", "Java library", new Hyperlink("https://fazecast.github.io/jSerialComm/")));
         resources.add(new Resource("daisy", "an image", new Hyperlink("https://cdn.shopify.com/s/files/1/2319/4521/files/pexels-pixabay-67857_0fd12879-bc6e-4699-a969-812cf1386085.jpg?v=1651115165&width=3840")));
@@ -69,12 +73,15 @@ public class CreditsController {
         int rowIndex = 1;
         for (Resource resource : resources) {
             Label nameLabel = new Label(resource.getName());
+            nameLabel.setWrapText(true);
             nameLabel.setAlignment(Pos.CENTER_LEFT);
 
             Label description = new Label(resource.getDescription());
+            description.setWrapText(true);
             description.setAlignment(Pos.CENTER);
 
             Label link = new Label(resource.getLink().getText());
+            link.setWrapText(true);
             link.setAlignment(Pos.CENTER_RIGHT);
 
             sourceGrid.addRow(rowIndex, nameLabel, description, link);
