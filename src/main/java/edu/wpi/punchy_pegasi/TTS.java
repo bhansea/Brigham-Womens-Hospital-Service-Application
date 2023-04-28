ec4bc06eb72be4e77462fa75b89a8cf571ce014d
Coops530 <cooper530@users.noreply.github.com>
2023-04-28 09:18:56 -0400

    Text to speech implemented in app (use TTS.speak(text)).

---
+++ src/main/java/edu/wpi/punchy_pegasi/TTS.java
@@ -0,0 +1,18 @@
+package edu.wpi.punchy_pegasi;
+
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+
+public class TTS {
+
+    private static final String VOICE_NAME = "kevin16";
+
+    public static void speak(String text) {
+        System.setProperty("freetts.voices","com.sun.speech.freetts.en.us.cmu_time_awb.AlanVoiceDirectory");
+        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
+        VoiceManager voiceManager = VoiceManager.getInstance();
+        Voice voice = voiceManager.getVoice(VOICE_NAME);
+        voice.allocate();
+        voice.speak(text);
+    }
+}
