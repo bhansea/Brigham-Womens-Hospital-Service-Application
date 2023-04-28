451e1e338b31624e9ba6c4a29d3f9ddff67810c7
bhansea <97768606+bhansea@users.noreply.github.com>
2023-04-28 15:34:30 -0400

    finalized textOverlay and replaced arnav's picture

--- src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/AboutController.java
+++ src/main/java/edu/wpi/punchy_pegasi/frontend/controllers/AboutController.java
@@ -7,6 +7,7 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -34,7 +35,7 @@ public class AboutController {
         TeamMember brittany = new TeamMember("Brittany Ficarra", "FE Software Engineer", "bcficarra@wpi.edu", "", "", new ImageView("edu/wpi/punchy_pegasi/frontend/assets/headshots/brittany.PNG"));
         TeamMember brandon = new TeamMember("Brandon Luong", "BE Software Engineer", "bvluong@wpi.edu", "", "", new ImageView("edu/wpi/punchy_pegasi/frontend/assets/headshots/brandon.PNG"));
         TeamMember cooper = new TeamMember("Cooper Mann", "Scrum Master", "cpmann@wpi.edu", "", "", new ImageView("edu/wpi/punchy_pegasi/frontend/assets/headshots/cooper.PNG"));
-        TeamMember arnav = new TeamMember("Arnav Mishra", "BE Assistant Lead Software Engineer", "amishra2@wpi.edu", "", "", new ImageView("edu/wpi/punchy_pegasi/frontend/assets/headshots/arnav.PNG"));
+        TeamMember arnav = new TeamMember("Arnav Mishra", "BE Assistant Lead Software Engineer", "amishra2@wpi.edu", "", "", new ImageView("edu/wpi/punchy_pegasi/frontend/assets/headshots/arnav.jpg"));
         TeamMember ryan = new TeamMember("Ryan Nguyen", "FE Assistant Lead Software Engineer", "rnguyen@wpi.edu", "", "", new ImageView("edu/wpi/punchy_pegasi/frontend/assets/headshots/ryan.PNG"));
         TeamMember piper = new TeamMember("Piper O'Connell", "Project Manager", "proconnell@wpi.edu", "", "", new ImageView("edu/wpi/punchy_pegasi/frontend/assets/headshots/piper.PNG"));
         TeamMember gabe = new TeamMember("Gabe Ward", "Product Owner", "gward@wpi.edu", "", "", new ImageView("edu/wpi/punchy_pegasi/frontend/assets/headshots/gabe.PNG"));
@@ -74,11 +75,6 @@ public class AboutController {
             this.funFact = funFact;
             this.image = image;
 
-
-//            StackPane.setAlignment(textOverlay, Pos.BOTTOM_LEFT);
-//            StackPane.setAlignment(nameLabel, Pos.BOTTOM_LEFT);
-//            StackPane.setAlignment(positionLabel, Pos.BOTTOM_LEFT);
-
             this.image.setFitWidth(250);
             this.image.setFitHeight(250);
             this.image.setPreserveRatio(false);
@@ -88,12 +84,31 @@ public class AboutController {
 
             nameLabel = new Label(this.name);
             nameLabel.getStyleClass().add("info-about-team-member-name");
+
+
+            DropShadow dropShadow = new DropShadow(5, Color.BLACK);
             positionLabel = new Label(this.position);
             positionLabel.getStyleClass().add("info-about-team-member-position");
+            positionLabel.setEffect(dropShadow);
+
+
+
             textOverlay = new VBox(nameLabel, positionLabel);
             textOverlay.setPadding(new Insets(0, 0, 5, 5));
             stackpane.getChildren().add(textOverlay);
             textOverlay.setAlignment(Pos.BOTTOM_LEFT);
+//
+//            stackpane.setOnMouseEntered(event -> {
+//                hiddenTextOverlay.setVisible(true);
+//                stackpane.setStyle("-fx-background-color: rgba(0, 0, 0, 0.5);");
+//                image.setOpacity(0.5);
+//            });
+//
+//            stackpane.setOnMouseExited(event -> {
+//                hiddenTextOverlay.setVisible(false);
+//                stackpane.setStyle("-fx-background-color: transparent;");
+//                image.setOpacity(1.0);
+//            });
         }
     }
 }
