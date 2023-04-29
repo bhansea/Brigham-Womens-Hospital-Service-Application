b97b00f58d918ee3b995666af71c7819f7b24731
Coops530 <cooper530@users.noreply.github.com>
2023-04-28 20:21:39 -0400

    Updated use of static variables.

--- src/main/java/edu/wpi/punchy_pegasi/TTS.java
+++ src/main/java/edu/wpi/punchy_pegasi/TTS.java
@@ -6,12 +6,12 @@ import com.sun.speech.freetts.VoiceManager;
 public class TTS {
 
     private static final String VOICE_NAME = "kevin16";
+    private static final VoiceManager voiceManager = VoiceManager.getInstance();
+    private static final Voice voice = voiceManager.getVoice(VOICE_NAME);
 
     public static void speak(String text) {
         System.setProperty("freetts.voices","com.sun.speech.freetts.en.us.cmu_time_awb.AlanVoiceDirectory");
         System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
-        VoiceManager voiceManager = VoiceManager.getInstance();
-        Voice voice = voiceManager.getVoice(VOICE_NAME);
         voice.setRate(170);
         voice.setPitch(120);
         voice.allocate();
